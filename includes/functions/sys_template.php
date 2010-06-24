f63735a585421663e0da7b960ecde2cf8b437b92
Gorlum <gorlum@triolan.ua>
2010-06-24 14:01:36 +0300

    SYS: Removed some debug info

--- includes/functions/sys_template.php
+++ includes/functions/sys_template.php
@@ -5,10 +5,7 @@ function displayP($template){
   foreach($template->parse as $key => $data)
     $template->assign_var($key, $data); // This is an example of assigning a template variable
 
-  $template->assign_var('HELLO_WORLD', 'Hello world'); // This is an example of assigning a template variable
-
   $template->display('body');
-  pr();
 }
 
 function parsetemplate ($template, $array) {
@@ -26,11 +23,6 @@ function parsetemplate ($template, $array) {
     $replace[] = '( ( isset($array[\'\1\']) ) ? $array[\'\1\'] : \'\' );';
 
     return preg_replace($search, $replace, $template);
-  /*
-    $template = preg_replace('#\{L_([a-z0-9\-_]*?)\[([a-z0-9\-_]*?)\]\}#Ssie', '( ( isset($lang[\'\1\'][\'\2\']) ) ? $lang[\'\1\'][\'\2\'] : \'\' );', $template);
-    $template = preg_replace('#\{L_([a-z0-9\-_]*?)\}#Ssie', '( ( isset($lang[\'\1\']) ) ? $lang[\'\1\'] : \'\' );', $template);
-    return preg_replace('#\{([a-z0-9\-_]*?)\}#Ssie', '( ( isset($array[\'\1\']) ) ? $array[\'\1\'] : \'\' );', $template);
-  */
   }
 }
 
