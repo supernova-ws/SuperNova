67df93eba538e827fdcc26b4528fdfb495658c73
Gorlum <gorlum@triolan.ua>
2010-06-28 21:43:36 +0300

    INS: Updated UPDATE script

--- install/update.php
+++ install/update.php
@@ -19,13 +19,9 @@ if ( $user['authlevel'] >= 3 ) {
   $game_version = doquery("SELECT `config_value` FROM {{config}} WHERE `config_name` = 'dbVersion';", '', true);
   switch($game_version['config_value']){
     case '':
-      doquery("
-        UPDATE `{{planets}}` AS lu
-          LEFT JOIN `{{planets}}` AS pl
-            ON pl.galaxy=lu.galaxy AND pl.system=lu.system AND pl.planet=lu.planet AND pl.planet_type=1
-        SET lu.parent_planet=pl.id
-        WHERE lu.planet_type=3;
-      ", '', true);
+      doquery("UPDATE `{{planets}}` AS lu
+          LEFT JOIN `{{planets}}` AS pl ON pl.galaxy=lu.galaxy AND pl.system=lu.system AND pl.planet=lu.planet AND pl.planet_type=1
+        SET lu.parent_planet=pl.id WHERE lu.planet_type=3;");
       $newVersion = 1;
       set_time_limit(30);
     case 1:
@@ -34,6 +30,7 @@ if ( $user['authlevel'] >= 3 ) {
     doquery("UPDATE {{config}} SET `config_value` = '{$newVersion}' WHERE `config_name` = 'dbVersion';", '', true);
     if(!mysql_affected_rows())
       doquery("INSERT INTO {{config}} SET `config_value` = '{$newVersion}', `config_name` = 'dbVersion';", '', true);
+    $config->dbVersion = $newVersion;
   };
 }
 
