ce2f44a5e27467818a5fdbcc8713085e5cd410c4
Gorlum <gorlum@triolan.ua>
2010-07-16 03:27:50 +0300

    SYS/UPG: Added code to create parent_planet, debris_metal, debris_crystal fields to planet table

--- install/update.php
+++ install/update.php
@@ -31,6 +31,12 @@ if ( $user['authlevel'] >= 3 ) {
   $config->db_loadItem('db_version');
   switch(intval($config->db_version)){
     case 0:
+      if(!$tables['planets']['parent_planet'])
+        mysql_query(
+          "ALTER TABLE {$config->db_prefix}planets
+            ADD `parent_planet` bigint(11) unsigned DEFAULT '0',
+            ADD KEY `i_parent_planet` (`parent_planet`)
+            ;");
       doquery("UPDATE `{{planets}}` AS lu
           LEFT JOIN `{{planets}}` AS pl ON pl.galaxy=lu.galaxy AND pl.system=lu.system AND pl.planet=lu.planet AND pl.planet_type=1
         SET lu.parent_planet=pl.id WHERE lu.planet_type=3;");
@@ -40,36 +46,46 @@ if ( $user['authlevel'] >= 3 ) {
     case 1:
       if(!$tables['counter']){
         mysql_query(
-"CREATE TABLE `{$config->db_prefix}counter` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `time` int(11) NOT NULL DEFAULT '0',
-  `page` varchar(255) CHARACTER SET utf8 DEFAULT '0',
-  `user_id` bigint(11) DEFAULT '0',
-  `ip` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `i_user_id` (`user_id`),
-  KEY `i_ip` (`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+          "CREATE TABLE `{$config->db_prefix}counter` (
+            `id` bigint(11) NOT NULL AUTO_INCREMENT,
+            `time` int(11) NOT NULL DEFAULT '0',
+            `page` varchar(255) CHARACTER SET utf8 DEFAULT '0',
+            `user_id` bigint(11) DEFAULT '0',
+            `ip` varchar(15) DEFAULT NULL,
+            PRIMARY KEY (`id`),
+            KEY `i_user_id` (`user_id`),
+            KEY `i_ip` (`ip`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
       };
       $newVersion = 2;
+      set_time_limit(30);
 
     case 2:
-
-      // NEED CONVERTER FROM "LUNAS" TO "PLANETS"
-      // NEED TO ADD `parent_planet` field to `planets` table
-
       if($tables['lunas'])
         mysql_query("DROP TABLE {$config->db_prefix}lunas;");
       $newVersion = 3;
+      set_time_limit(30);
 
     case 3:
       if(!$tables['counter']['url'])
         mysql_query("ALTER TABLE {$config->db_prefix}counter ADD `url` varchar(255) CHARACTER SET utf8 DEFAULT '';");
       $newVersion = 4;
+      set_time_limit(30);
 
     case 4:
+      if(!$tables['planets']['debris_metal'])
+        mysql_query(
+          "ALTER TABLE {$config->db_prefix}planets
+             ADD `debris_metal` bigint(11) unsigned DEFAULT '0'
+            ;");
+      set_time_limit(30);
 
-      // NEED TO ADD `debris_metal` and `debris_crystal` fields to `planets` table
+      if(!$tables['planets']['debris_crystal'])
+        mysql_query(
+          "ALTER TABLE {$config->db_prefix}planets
+             ADD `debris_crystal` bigint(11) unsigned DEFAULT '0'
+            ;");
+      set_time_limit(30);
 
       doquery('UPDATE `{{planets}}`
         LEFT JOIN `{{galaxy}}` ON {{galaxy}}.id_planet = {{planets}}.id
@@ -78,14 +94,16 @@ if ( $user['authlevel'] >= 3 ) {
         {{planets}}.debris_crystal = {{galaxy}}.crystal
       WHERE {{galaxy}}.metal>0 OR {{galaxy}}.crystal>0;');
       $newVersion = 5;
+      set_time_limit(30);
 
     case 5:
-      // mysql_query("DROP TABLE IF EXISTS `{$config->db_prefix}galaxy`;");
-      // $newVersion = 6;
+      mysql_query("DROP TABLE IF EXISTS `{$config->db_prefix}galaxy`;");
+      $newVersion = 6;
 
     case 6:
 
   };
+
   if($newVersion){
     $config->db_version = $newVersion;
     $config->db_saveItem('db_version');
