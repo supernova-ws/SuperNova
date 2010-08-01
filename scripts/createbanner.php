3ef0ad2fc8a41a52605fc626149e86d951804bb8
Gorlum <gorlum@triolan.ua>
2010-08-01 19:28:00 +0300

    Misc: Making copy of banner.php as scripts\createbanner.php to retain old-styled links

---
+++ scripts/createbanner.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* banner.php
+* @version 1.0s - Security checks by Gorlum for http://supernova.ws
+* @version 1.0
+*
+* Simple wrapper for INT_createBanner.php
+* Create banner or userbar
+* banner.php?id=<userid>&type=<banner|userbar>&format=<png>
+*
+* @copyright 2010 by Gorlum for http://supernova.ws
+*
+*/
+
+define('INSIDE' , true);
+define('INSTALL' , false);
+
+$ugamela_root_path = './';
+
+include($ugamela_root_path . 'extension.inc');
+include($ugamela_root_path . 'common.' . $phpEx);
+
+includeLang('overview');
+
+$id = intval($_GET['id']);
+$type = SYS_mysqlSmartEscape($_GET['type']);
+$format = SYS_mysqlSmartEscape($_GET['format']);
+
+INT_createBanner($id, $type, $format);
+?>
