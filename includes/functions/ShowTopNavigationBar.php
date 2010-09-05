1973955082c385b2ec6bbaaa6718244262412dc2
Gorlum <gorlum@triolan.ua>
2010-09-05 07:50:29 +0300

    INT: Top bar: Full rewrote. Using sn_timer.js for resource counters. Fixed bug in planet_resource_update.php which makes incorrect _perhour be wrote to `planets` table

--- includes/functions/ShowTopNavigationBar.php
+++ includes/functions/ShowTopNavigationBar.php
@@ -3,140 +3,77 @@
 /**
  * ShowTopNavigationBar.php
  *
- * @version 1.0s - Security checked for SQL-injection by Gorlum for http://supernova.ws
+ * @version 2.0 - Security checked for SQL-injection by Gorlum for http://supernova.ws
+ *   [+] Complies with PCG
+ *   [+] Utilize PTE
+ *   [+] Heavy optimization
+ * @version 1.1 - Security checked for SQL-injection by Gorlum for http://supernova.ws
  * @version 1
  * @copyright 2008 By Chlorel for XNova
  */
 
-function ShowTopNavigationBar ( $CurrentUser, $CurrentPlanet ) {
-  global $lang, $_GET, $game_config;
+function ShowTopNavigationBar ( $CurrentUser, $CurrentPlanet )
+{
+  global $_GET, $time_now, $dpath;
 
-  $GET_mode = SYS_mysqlSmartEscape($_GET['mode']);
 
-  if ($CurrentUser) {
-    if ( !$CurrentPlanet ) {
-      $CurrentPlanet = doquery("SELECT * FROM `{{table}}` WHERE `id` = '". $CurrentUser['current_planet'] ."';", 'planets', true);
+  if ($CurrentUser)
+  {
+    $GET_mode = SYS_mysqlSmartEscape($_GET['mode']);
+
+    $template       = gettemplate('topnav', true);
+
+    if (!$CurrentPlanet)
+    {
+      $CurrentPlanet = doquery("SELECT * FROM `{{planets}}` WHERE `id` = '{$CurrentUser['current_planet']}';", '', true);
     }
 
-    // Actualisation des ressources de la planete
-    PlanetResourceUpdate ( $CurrentUser, $CurrentPlanet, time() );
+    PlanetResourceUpdate ( $CurrentUser, $CurrentPlanet, $time_now );
 
-    $NavigationTPL       = gettemplate('topnav');
-
-    $dpath               = (!$CurrentUser["dpath"]) ? DEFAULT_SKINPATH : $CurrentUser["dpath"];
-    $parse               = $lang;
-    $parse['dpath']      = $dpath;
-    $parse['image']      = $CurrentPlanet['image'];
-
-    // Genearation de la combo des planetes du joueur
-    $parse['planetlist'] = '';
     $ThisUsersPlanets    = SortUserPlanets ( $CurrentUser );
     while ($CurPlanet = mysql_fetch_array($ThisUsersPlanets)) {
-      if ($CurPlanet["destruyed"] == 0) {
-        $parse['planetlist'] .= "\n<option ";
-        if ($CurPlanet['id'] == $CurrentUser['current_planet']) {
-          // Bon puisque deja on s'y trouve autant le marquer
-          $parse['planetlist'] .= "selected=\"selected\" ";
-        }
-        $parse['planetlist'] .= "value=\"?cp=".$CurPlanet['id']."";
-        $parse['planetlist'] .= "&amp;mode=".$GET_mode;
-        $parse['planetlist'] .= "&amp;re=0\">";
-
-        // Nom et coordonnées de la planete
-        $parse['planetlist'] .= "".$CurPlanet['name'];
-        $parse['planetlist'] .= "&nbsp;[".$CurPlanet['galaxy'].":";
-        $parse['planetlist'] .= "".$CurPlanet['system'].":";
-        $parse['planetlist'] .= "".$CurPlanet['planet'];
-        $parse['planetlist'] .= "]&nbsp;&nbsp;</option>";
+      if (!$CurPlanet['destruyed'])
+      {
+        $template->assign_block_vars('topnav_planets', array(
+          'ID'     => $CurPlanet['id'],
+          'NAME'   => $CurPlanet['name'],
+          'COORDS' => INT_makeCoordinates($CurPlanet),
+          'SELECTED' => $CurPlanet['id'] == $CurrentUser['current_planet'] ? ' selected' : '',
+        ));
       }
     }
 
-    $energy = pretty_number($CurrentPlanet["energy_max"] + $CurrentPlanet["energy_used"]) . "/" . pretty_number($CurrentPlanet["energy_max"]);
-    // Energie
-    if ($CurrentPlanet["energy_used"] > $CurrentPlanet["energy_max"]) {
-      $parse['energy'] = colorRed($energy);
-    } else {
-      $parse['energy'] = $energy;
-    }
+    $template->assign_vars(array(
+      'dpath'      => $dpath,
+      'TIME_NOW' => $time_now,
 
-    // Metal
-    $metal = pretty_number($CurrentPlanet["metal"]);
-    if (($CurrentPlanet["metal"] > $CurrentPlanet["metal_max"])) {
-      $parse['metal'] = colorRed($metal);
-    } else {
-      $parse['metal'] = $metal;
-    }
-    // Cristal
-    $crystal = pretty_number($CurrentPlanet["crystal"]);
-    if (($CurrentPlanet["crystal"] > $CurrentPlanet["crystal_max"])) {
-      $parse['crystal'] = colorRed($crystal);
-    } else {
-      $parse['crystal'] = $crystal;
-    }
-    // Deuterium
-    $deuterium = pretty_number($CurrentPlanet["deuterium"]);
-    if (($CurrentPlanet["deuterium"] > $CurrentPlanet["deuterium_max"])) {
-      $parse['deuterium'] = colorRed($deuterium);
-    } else {
-      $parse['deuterium'] = $deuterium;
-    }
+      'TOPNAV_CURRENT_PLANET' => $CurrentUser['current_planet'],
+      'TOPNAV_MODE' => $GET_mode,
 
-    // Max Energie
-    $res = $CurrentPlanet['energy_max'] - $CurrentPlanet['energy_used'];
-    $parse['energy_total'] = colorNumber(pretty_number($res));
-    $parse['energy_max'] = pretty_number($CurrentPlanet["energy_max"]);
+      'TOPNAV_METAL' => round($CurrentPlanet["metal"]),
+      'TOPNAV_METAL_MAX' => round($CurrentPlanet["metal_max"]),
+      'TOPNAV_METAL_MAX_TEXT' => pretty_number($CurrentPlanet["metal_max"], true, -$CurrentPlanet["metal"]),
+      'TOPNAV_METAL_PERHOUR' => round($CurrentPlanet["metal_perhour"]),
 
+      'TOPNAV_CRYSTAL' => round($CurrentPlanet["crystal"]),
+      'TOPNAV_CRYSTAL_MAX' => round($CurrentPlanet["crystal_max"]),
+      'TOPNAV_CRYSTAL_MAX_TEXT' => pretty_number($CurrentPlanet["crystal_max"], true, -$CurrentPlanet["crystal"]),
+      'TOPNAV_CRYSTAL_PERHOUR' => round($CurrentPlanet["crystal_perhour"]),
 
-// Metal maximo
-if ($CurrentPlanet["metal_max"] < $CurrentPlanet["metal"]) {
-  $parse['metal_max'] = '<font color="#ff0000">';
-} else {
-  $parse['metal_max'] = '<font color="#00ff00">';
-}
-$parse['metal_max'] .= pretty_number($CurrentPlanet["metal_max"] / 1) . " {$lang['']}</font>";
+      'TOPNAV_DEUTERIUM' => round($CurrentPlanet["deuterium"]),
+      'TOPNAV_DEUTERIUM_MAX' => round($CurrentPlanet["deuterium_max"]),
+      'TOPNAV_DEUTERIUM_MAX_TEXT' => pretty_number($CurrentPlanet["deuterium_max"], true, -$CurrentPlanet["deuterium"]),
+      'TOPNAV_DEUTERIUM_PERHOUR' => round($CurrentPlanet["deuterium_perhour"]),
 
-// Cristal maximo
-if ($CurrentPlanet["crystal_max"] < $CurrentPlanet["crystal"]) {
-  $parse['crystal_max'] = '<font color="#ff0000">';
-} else {
-  $parse['crystal_max'] = '<font color="#00ff00">';
-}
-$parse['crystal_max'] .= pretty_number($CurrentPlanet["crystal_max"] / 1) . " {$lang['']}";
+      'TOPNAV_DARK_MATTER' => pretty_number($CurrentUser['rpg_points']),
 
-// Deuterio maximo
-if ($CurrentPlanet["deuterium_max"] < $CurrentPlanet["deuterium"]) {
-  $parse['deuterium_max'] = '<font color="#ff0000">';
-} else {
-  $parse['deuterium_max'] = '<font color="#00ff00">';
-}
-$parse['deuterium_max'] .= pretty_number($CurrentPlanet["deuterium_max"] / 1) . " {$lang['']}";
+      'ENERGY_BALANCE' => pretty_number($CurrentPlanet['energy_max'] - $CurrentPlanet['energy_used'], true, 0),
+      'ENERGY_MAX' => pretty_number($CurrentPlanet['energy_max']),
 
-$parse['metal_perhour'] .= $CurrentPlanet["metal_perhour"];
-$parse['crystal_perhour'] .= $CurrentPlanet["crystal_perhour"];
-$parse['deuterium_perhour'] .= $CurrentPlanet["deuterium_perhour"];
+      'TOPNAV_MESSAGES'    => $CurrentUser['new_message'],
+    ));
 
-$parse['metalh'] .= round($CurrentPlanet["metal"]);
-$parse['crystalh'] .= round($CurrentPlanet["crystal"]);
-$parse['deuteriumh'] .= round($CurrentPlanet["deuterium"]);
-
-$parse['metal_mmax'] .= $CurrentPlanet["metal_max"];
-$parse['crystal_mmax'] .= $CurrentPlanet["crystal_max"];
-$parse['deuterium_mmax'] .= $CurrentPlanet["deuterium_max"];
-
-
-
-    // Message
-    if ($CurrentUser['new_message'] > 0) {
-      $parse['message'] = "<a href=\"messages.php\"><blink>[ ". $CurrentUser['new_message'] ." ]</blink></a>";
-    } else {
-      $parse['message'] = "0";
-    }
-    ///dark mater
-     $parse['dark_matter'] = pretty_number($CurrentUser['rpg_points']);
-    // Le tout passe dans la template
-    $parse['resource_multiplier'] = ($game_config['resource_multiplier']);
-    $parse['rpg_geologue'] = ( 1 + ( $CurrentUser['rpg_geologue']  * 0.05 ));
-    $TopBar = parsetemplate( $NavigationTPL, $parse);
+    $TopBar = parsetemplate( $template, $parse);
   } else {
     $TopBar = "";
   }
