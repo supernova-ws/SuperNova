3a2a175e52aa3cd150a09425d2d911fd536a96ed
Gorlum <gorlum@supernova.ws>
2010-10-11 16:23:58 +0300

    Fix: Moved update.php call from init.inc to common.php to work only with authrized user. Also made some changes to prevent duplicate stat update and update.php launch

--- scheduler.php
+++ scheduler.php
@@ -1,48 +1,55 @@
 <?php
 include_once('includes/init.inc');
 
-$nextStatUpdate = SYS_scheduleGetNextRun($config->stats_schedule, $config->stats_lastUpdated, $time_now);
-
-includeLang('admin');
-
-if($_SERVER['HTTP_REFERER'] == "http://{$_SERVER['HTTP_HOST']}/admin/statbuilder.php")
+if(!$config->db_loadItem('var_scheduler_active'))
 {
-  $isAdminRequest = true;
-  $nextStatUpdate = time();
-}
+  $config->db_saveItem('var_scheduler_active', true);
 
-if($nextStatUpdate>$config->stats_lastUpdated)
-{
-  if($isAdminRequest)
+  $nextStatUpdate = SYS_scheduleGetNextRun($config->stats_schedule, $config->stats_lastUpdated, $time_now);
+
+  includeLang('admin');
+
+  if($_SERVER['HTTP_REFERER'] == "http://{$_SERVER['HTTP_HOST']}/admin/statbuilder.php")
   {
-    $msg = 'admin request';
+    $isAdminRequest = true;
+    $nextStatUpdate = time();
+  }
+
+  if($nextStatUpdate>$config->stats_lastUpdated)
+  {
+    if($isAdminRequest)
+    {
+      $msg = 'admin request';
+    }
+    else
+    {
+      $msg = 'scheduler';
+    };
+    $msg .= '. Config->stats_lastUpdated = ' . date(DATE_TIME, $config->stats_lastUpdated) . ', nextStatUpdate = ' . date(DATE_TIME, $nextStatUpdate);
+    $config->db_saveItem('stats_lastUpdated', $nextStatUpdate);
+    $debug->warning("Running stat updates: {$msg}", 'Stat update', 100);
+
+    $totaltime = microtime(true);
+    SYS_statCalculate();
+    $totaltime = microtime(true) - $totaltime;
+
+    $msg = "{$lang['adm_done']}: {$totaltime} {$lang['sys_sec']}";
+    $debug->warning("Stat update complete: {$msg}", 'Stat update', 101);
   }
   else
   {
-    $msg = 'scheduler';
-  };
-  $msg .= '. Config->stats_lastUpdated = ' . date(DATE_TIME, $config->stats_lastUpdated) . ', nextStatUpdate = ' . date(DATE_TIME, $nextStatUpdate);
-  $config->db_saveItem('stats_lastUpdated', $nextStatUpdate);
-  $debug->warning("Running stat updates: {$msg}", 'Stat update', 100);
+  //  $msg = $lang['adm_schedule_none'];
+  }
 
-  $totaltime = microtime(true);
-  SYS_statCalculate();
-  $totaltime = microtime(true) - $totaltime;
+  if($msg)
+  {
+    $msg = iconv('CP1251', 'UTF-8', $msg);
+    $xml = "<ratings><message>{$msg}</message></ratings>";
 
-  $msg = "{$lang['adm_done']}: {$totaltime} {$lang['sys_sec']}";
-  $debug->warning("Stat update complete: {$msg}", 'Stat update', 101);
-}
-else
-{
-//  $msg = $lang['adm_schedule_none'];
-}
+    header('Content-type: text/xml');
+    echo $xml;
+  }
 
-if($msg)
-{
-  $msg = iconv('CP1251', 'UTF-8', $msg);
-  $xml = "<ratings><message>{$msg}</message></ratings>";
-
-  header('Content-type: text/xml');
-  echo $xml;
+  $config->db_saveItem('var_scheduler_active', false);
 }
 ?>
