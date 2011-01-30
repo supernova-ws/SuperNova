f44d15ad837319af8934e0c9414d8e5b105e64d7
Gorlum <gorlum@supernova.ws>
2011-01-30 17:19:58 +0200

    misc-INT: Replaced placeholders in top navbar with actual values

--- includes/template.php
+++ includes/template.php
@@ -284,8 +284,9 @@ function ShowTopNavigationBar ( $CurrentUser, $CurrentPlanet )
 
     $template->assign_vars(array(
       'dpath'      => $dpath,
-      'TIME_NOW' => $time_now,
-      'TIME_TEXT'          => "$day_of_week, $day $month $year {$lang['top_of_year']},",
+      'TIME_NOW'   => $time_now,
+      'DATE_TEXT'          => "$day_of_week, $day $month $year {$lang['top_of_year']},",
+      'TIME_TEXT'          => "{$hour}:{$min}:{$sec}",
 
       'USERS_ONLINE'         => mysql_num_rows($OnlineUsersNames2),
       'USERS_TOTAL'          => $config->users_amount,
@@ -294,21 +295,21 @@ function ShowTopNavigationBar ( $CurrentUser, $CurrentPlanet )
       'TOPNAV_MODE' => $GET_mode,
 
       'TOPNAV_METAL' => round($CurrentPlanet["metal"], 2),
-      'TOPNAV_METAL_TEXT' => pretty_number($CurrentPlanet["metal"], 2),
       'TOPNAV_METAL_MAX' => round($CurrentPlanet["metal_max"]),
       'TOPNAV_METAL_PERHOUR' => round($CurrentPlanet["metal_perhour"], 5),
+      'TOPNAV_METAL_TEXT' => pretty_number($CurrentPlanet["metal"], 2),
       'TOPNAV_METAL_MAX_TEXT' => pretty_number($CurrentPlanet["metal_max"], 2, -$CurrentPlanet["metal"]),
 
       'TOPNAV_CRYSTAL' => round($CurrentPlanet["crystal"], 2),
-      'TOPNAV_CRYSTAL_TEXT' => pretty_number($CurrentPlanet["crystal"], 2),
-      'TOPNAV_CRYSTAL_PERHOUR' => round($CurrentPlanet["crystal_perhour"], 5),
       'TOPNAV_CRYSTAL_MAX' => round($CurrentPlanet["crystal_max"]),
+      'TOPNAV_CRYSTAL_PERHOUR' => round($CurrentPlanet["crystal_perhour"], 5),
+      'TOPNAV_CRYSTAL_TEXT' => pretty_number($CurrentPlanet["crystal"], 2),
       'TOPNAV_CRYSTAL_MAX_TEXT' => pretty_number($CurrentPlanet["crystal_max"], 2, -$CurrentPlanet["crystal"]),
 
       'TOPNAV_DEUTERIUM' => round($CurrentPlanet["deuterium"], 2),
-      'TOPNAV_DEUTERIUM_TEXT' => pretty_number($CurrentPlanet["deuterium"], 2),
-      'TOPNAV_DEUTERIUM_PERHOUR' => round($CurrentPlanet["deuterium_perhour"], 5),
       'TOPNAV_DEUTERIUM_MAX' => round($CurrentPlanet["deuterium_max"]),
+      'TOPNAV_DEUTERIUM_PERHOUR' => round($CurrentPlanet["deuterium_perhour"], 5),
+      'TOPNAV_DEUTERIUM_TEXT' => pretty_number($CurrentPlanet["deuterium"], 2),
       'TOPNAV_DEUTERIUM_MAX_TEXT' => pretty_number($CurrentPlanet["deuterium_max"], 2, -$CurrentPlanet["deuterium"]),
 
       'TOPNAV_DARK_MATTER' => pretty_number($CurrentUser['rpg_points']),
