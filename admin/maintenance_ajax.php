86ca24b612cd07b39ab81b521295d2d2bf29b732
Gorlum <gorlum@supernova.ws>
2011-01-31 13:28:54 +0200

    fix-ALI: Fixed wrong table name in update ally players query

--- admin/maintenance_ajax.php
+++ admin/maintenance_ajax.php
@@ -19,7 +19,7 @@ $totaltime = microtime(true);
 $msg = '<div align="left"><ul>';
 
 // Delete users inactive for 45 days
-$query = doquery("SELECT * FROM {{table}} WHERE `onlinetime` < unix_timestamp(now())-(60*60*24*45);", "users");
+$query = doquery("SELECT * FROM {{users}} WHERE `onlinetime` < unix_timestamp(now()) - ( 60 * 60 * 24 * 45);");
   $rows += mysql_num_rows($query);
   while($u = mysql_fetch_assoc($query)){
     set_time_limit(30);
@@ -29,21 +29,25 @@ $query = doquery("SELECT * FROM {{table}} WHERE `onlinetime` < unix_timestamp(no
 $msg .= sprintf($lang['adm_inactive_removed'], $rows);
 
 $ques = array(
-  'DELETE FROM {{messages}} WHERE message_time<unix_timestamp(now())-(60*60*24*35);',
-  'DELETE FROM {{rw}} WHERE time<unix_timestamp(now())-(60*60*24*14);',
-  'DELETE FROM {{chat}} WHERE timestamp<unix_timestamp( now())-(60*60*24*14);',
-
+  'DELETE FROM {{messages}} WHERE message_time < unix_timestamp(now()) - (60 * 60 * 24 * 35);',
+  'DELETE FROM {{rw}} WHERE time < unix_timestamp(now()) - (60 * 60 * 24 * 14);',
+  'DELETE FROM {{chat}} WHERE timestamp < unix_timestamp(now()) - (60 * 60 * 24 * 14);',
+/*
   'DELETE FROM {{planets}} WHERE id_owner not in (select id from {{users}});',
   'DELETE FROM {{messages}} WHERE message_owner not in (select id from {{users}});',
   'DELETE FROM {{rw}} WHERE id_owner1 not in (select id from {{users}});',
   'DELETE FROM {{rw}} WHERE id_owner2 not in (select id from {{users}});',
+*/
+  'DELETE {{planets}}.* FROM {{planets}} LEFT JOIN {{users}} ON {{users}}.id = {{planets}}.id_owner WHERE {{users}}.id IS NULL;',
+  'DELETE {{messages}}.* FROM {{messages}} LEFT JOIN {{users}} ON {{users}}.id = {{messages}}.message_owner WHERE {{users}}.id IS NULL;',
+  'DELETE {{rw}}.* FROM {{rw}} LEFT JOIN {{users}} AS u ON u.id = {{rw}}.id_owner1 WHERE u.id IS NULL;',
+  'DELETE {{rw}}.* FROM {{rw}} LEFT JOIN {{users}} AS u ON u.id = {{rw}}.id_owner2 WHERE u.id IS NULL;',
+
   'DELETE FROM {{alliance}} WHERE id not in (select ally_id from {{users}} group by ally_id);',
   "DELETE FROM {{statpoints}} WHERE stat_type=1 AND id_owner not in (select id from {{users}});",
 
   "UPDATE {{users}} SET ally_id = 0, ally_name='', ally_rank_id=0 WHERE ally_id not in (select id from {{alliance}});",
   "DELETE FROM {{statpoints}} WHERE stat_type=2 AND id_owner not in (select id from {{alliance}});",
-
-//    'DELETE FROM {{galaxy}} WHERE id_planet not in (select id from {{planets}});',
 );
 
 foreach($ques as $que) {
