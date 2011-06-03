3b79c34e6325b4619ac7afd0e226ff8786442dc4
Gorlum <gorlum@supernova.ws>
2011-06-03 14:01:48 +0300

    fix-FLT: Fixed error in mission Transport when destination planet not exist

--- includes/functions/flt_mission_transport.php
+++ includes/functions/flt_mission_transport.php
@@ -14,7 +14,7 @@ function flt_mission_transport($mission_data)
   $source_planet      = $mission_data['src_planet'];
   $destination_planet = $mission_data['dst_planet'];
 
-  if(!$destination_planet || !is_array($destination_planet))
+  if(!$destination_planet || !is_array($destination_planet) || !$destination_planet['id_owner'])
   {
     doquery("UPDATE {{fleets}} SET `fleet_mess` = 1 WHERE `fleet_id` = {$fleet_row['fleet_id']} LIMIT 1;");
     return CACHE_FLEET;
@@ -29,9 +29,9 @@ function flt_mission_transport($mission_data)
   global $lang;
 
   $StartName        = $source_planet['name'];
-  $StartOwner       = $source_planet['id_owner'];
+  $StartOwner       = $fleet_row['fleet_owner'];
   $TargetName       = $destination_planet['name'];
-  $TargetOwner      = $destination_planet['id_owner'];
+  $TargetOwner      = $fleet_row['fleet_target_owner'];
 
   $Message = sprintf( $lang['sys_tran_mess_owner'],
               $TargetName, uni_render_coordinates_href($fleet_row, 'fleet_end_', 3, ''),
