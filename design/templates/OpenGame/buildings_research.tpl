239e8aa8df504984a0206d5a1d03570f756220dd
Gorlum <gorlum@supernova.ws>
2011-07-22 19:14:36 +0300

    feat-INT-29d21: Reworked Research interface

--- design/templates/OpenGame/buildings_research.tpl
+++ design/templates/OpenGame/buildings_research.tpl
@@ -1,15 +1,68 @@
 <center>
-<font color="#ff0000">{noresearch}</font>
 <br />
-<table align=top>
-<tr>
-	<td>
-	<table width=530>
-	<tr>
-	{technolist}
-	</tr>
-	</table>
-	</td>
-</tr>
+<!-- IF MESSAGE --><span class="error">{MESSAGE}</span><!-- ENDIF -->
+<table width=530>
+  <!-- BEGIN production -->
+  <tr>
+    <td class="c_c">
+      <a href="infos.php?gid={production.ID}">
+        <img border=0 src="{dpath}gebaeude/{production.ID}.gif" align="top" width=120 height=120>
+      </a>
+    </td>
+    <td class="c_c">
+      <a href="infos.php?gid={production.ID}">{production.NAME}</a>
+      {production.LEVEL}<br>
+      {production.DESCRIPTION}<br>
+      <table width="100%" border=0>
+        <tr>
+          <td valign="middle" style="border: 0">
+            {L_ConstructionTime}:&nbsp;{production.TIME}
+            <table align="center">
+              <tr>
+                <th class=c_c width=45>{L_sys_resources}</th>
+                <th class=c_c width=65>{L_eco_price}</th>
+                <th class=c_c width=65>{L_eco_left}</th>
+              </tr>
+              <!-- IF production.METAL -->
+              <tr>
+                <th class="c_l">{L_sys_metal}</th>
+                <td class="c_r" width=65>{production.METAL_PRINT}</td>
+                <td class="c_r" width=65>{production.METAL_REST}</td>
+              </tr>
+              <!-- ENDIF -->
+              <!-- IF production.CRYSTAL -->
+              <tr>
+                <th class="c_l">{L_sys_crystal}</th>
+                <td class="c_r">{production.CRYSTAL_PRINT}</td>
+                <td class="c_r">{production.CRYSTAL_REST}</td>
+              </tr>
+              <!-- ENDIF -->
+              <!-- IF production.DEUTERIUM -->
+              <tr>
+                <th class="c_l">{L_sys_deuterium}</th>
+                <td class="c_r">{production.DEUTERIUM_PRINT}</td>
+                <td class="c_r">{production.DEUTERIUM_REST}</td>
+              </tr>
+              <!-- ENDIF -->
+              <!-- IF production.ENERGY -->
+              <tr>
+                <th class="c_l">{L_sys_energy}</th>
+                <td class="c_r">{production.ENERGY_PRINT}</td>
+                <td class="c_r">-</td>
+              </tr>
+              <!-- ENDIF -->
+            </table>
+          </td>
+          <td valign="middle" style="border: 0">
+            {production.TECH_LINK}
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <!-- BEGINELSE production -->
+  <tr>
+
+  </tr>
+  <!-- END production -->
 </table>
-</center>
--- includes/functions/eco_build_research.php
+++ includes/functions/eco_build_research.php
@@ -13,134 +13,49 @@ function eco_lab_is_building($config, $que)
   return $que['in_que_abs'][31] && !$config->BuildLabWhileRun ? true : false;
 }
 
-/**
- * GetRestPrice.php
- *
- * @version 1.0
- * @copyright 2008 By Chlorel for XNova
- */
-
-// Calcul du surplus de ressources disponible apres l'achat d'un Element (Batiment / Recherche / Defense / Vaisseau )
-// $user       -> Le Joueur lui meme
-// $planet     -> La planete sur laquelle l'Element doit etre construit
-// $Element    -> L'Element que l'on convoite
-// $userfactor -> true  pour un batiment ou une recherche
-// -> false pour une defense ou un vaisseau
-//
-// Reponse : Chaine de caractère mise en forme prete a etre affichée
-function GetRestPrice ($user, $planet, $Element, $userfactor = true) {
-  global $pricelist, $resource, $lang, $sn_data;
-
-  if ($userfactor) {
-    $level = ($planet[$resource[$Element]]) ? $planet[$resource[$Element]] : $user[$resource[$Element]];
-  }
-
-  $array = array(
-    RES_METAL        => $lang["sys_metal"],
-    RES_CRYSTAL        => $lang["sys_crystal"],
-    RES_DEUTERIUM        => $lang["sys_deuterium"],
-    'energy_max' => $lang["sys_energy"]
-    );
-
-  $fleet_list = flt_get_fleets_to_planet($planet);
-  $fleet_own_count = $fleet_list['own']['count'];
-
-  $text  = "<br><font color=\"#7f7f7f\">{$lang['Rest_ress']}: ";
-  $text1 = "";
-  foreach ($array as $res_id => $ResTitle) {
-    if(is_numeric($res_id))
-    {
-      $ResType = $sn_data[$res_id]['name'];
-    }
-    else
-    {
-      $ResType = $res_id;
-    }
-
-    if ($pricelist[$Element][$ResType] != 0) {
-      $text .= "{$ResTitle}: ";
-      if ($userfactor) {
-        $cost = floor($pricelist[$Element][$ResType] * pow($pricelist[$Element]['factor'], $level));
-      } else {
-        $cost = floor($pricelist[$Element][$ResType]);
-      }
-
-      $resource_left = $planet[$ResType] - $cost;
-      if ($resource_left < 0) {
-        $color_rgb = '127, 95, 96';
-      } else {
-        $color_rgb = '95, 127, 108';
-      }
-      $resource_left = pretty_number($resource_left);
-      $text .= "<b style=\"color: rgb({$color_rgb});\">{$resource_left}</b> ";
-
-      if($fleet_own_count)
-      {
-        $resource_left = $planet[$ResType] + $fleet_list['own']['total'][$res_id] - $cost;
-        if ($resource_left < 0) {
-          $color_rgb = '127, 95, 96';
-        } else {
-          $color_rgb = '95, 127, 108';
-        }
-        $resource_left = pretty_number($resource_left);
-        $text1 .= "{$ResTitle}: <b style=\"color: rgb({$color_rgb});\">{$resource_left}</b>, ";
-      }
-    }
-  }
-  $text .= '</font>';
-
-  if($fleet_own_count)
-  {
-    $text1 = substr($text1, 0, -2);
-    $text .= "<br><font color=\"#7f7f7f\">{$lang['Rest_ress_fleet']}: {$text1}</font>";
-  }
-
-  return $text;
-}
-
 // Page de Construction de niveau de Recherche
-// $CurrentPlanet -> Planete sur laquelle la construction est lancée
+// $planet -> Planete sur laquelle la construction est lancée
 //                   Parametre passé par adresse, cela permet de mettre les valeurs a jours
 //                   dans le programme appelant
 // $CurrentUser   -> Utilisateur qui a lancé la construction
 // $InResearch    -> Indicateur qu'il y a une Recherche en cours
 // $ThePlanet     -> Planete sur laquelle se realise la technologie eventuellement
-function ResearchBuildingPage (&$CurrentPlanet, $CurrentUser, $InResearch, $ThePlanet, $que) {
+function ResearchBuildingPage (&$planet, $CurrentUser, $InResearch, $ThePlanet, $que)
+{
   global $lang, $resource, $reslist, $dpath, $config, $sn_data;
 
   $sn_data_group_tech = $sn_data['groups']['tech'];
 
-  $TheCommand = SYS_mysqlSmartEscape($_GET['cmd']);
-  $Techno     = intval($_GET['tech']);
-
   $NoResearchMessage = "";
-  $bContinue         = true;
   // Deja est qu'il y a un laboratoire sur la planete ???
-  if ($CurrentPlanet[$resource[31]] == 0) {
+  if ($planet[$resource[31]] == 0)
+  {
     message($lang['no_laboratory'], $lang['Research']);
   }
+
   // Ensuite ... Est ce que la labo est en cours d'upgrade ?
-  if (eco_lab_is_building($config, $que)) {
+  if (eco_lab_is_building($config, $que))
+  {
     $NoResearchMessage = $lang['labo_on_update'];
-    $bContinue         = false;
   }
 
   // Boucle d'interpretation des eventuelles commandes
-  if (isset($TheCommand)) {
-    if (!$Techno) {
-      $debug->error("Buguser: ".$user['username']." (".$user['id'].")<br />Free research","Bug use");
-      die();
-    };
-
-    if ( is_numeric($Techno) ) {
-      if ( in_array($Techno, $reslist['tech']) ) {
+  $TheCommand = sys_get_param_str('cmd');
+  $Techno     = sys_get_param_int('tech');
+  if(isset($TheCommand))
+  {
+    if(is_numeric($Techno))
+    {
+      if ( in_array($Techno, $reslist['tech']) ) 
+      {
         // Bon quand on arrive ici ... On sait deja qu'on a une technologie valide
 
         if ( is_array ($ThePlanet) ) {
           $WorkingPlanet = $ThePlanet;
         } else {
-          $WorkingPlanet = $CurrentPlanet;
+          $WorkingPlanet = $planet;
         }
+
         switch($TheCommand){
           case 'cancel':
             if ($ThePlanet['b_tech_id'] == $Techno) {
@@ -149,26 +64,22 @@ function ResearchBuildingPage (&$CurrentPlanet, $CurrentUser, $InResearch, $TheP
               $WorkingPlanet['crystal']    += $costs['crystal'];
               $WorkingPlanet['deuterium']  += $costs['deuterium'];
               $WorkingPlanet['b_tech_id']   = 0;
-              $WorkingPlanet["b_tech"]      = 0;
+              $WorkingPlanet['b_tech']      = 0;
               $CurrentUser['b_tech_planet'] = 0;
               $UpdateData                   = true;
               $InResearch                   = false;
-              $QryUpdatePlanet2  = "UPDATE {{planets}} SET ";
-              $QryUpdatePlanet2 .= "`b_tech_id` = '".$WorkingPlanet['b_tech_id']."', ";
-              $QryUpdatePlanet2 .= "`b_tech` = '".$WorkingPlanet['b_tech']."', ";
-              $QryUpdatePlanet2 .= "`metal` = '".$WorkingPlanet['metal']."', ";
-              $QryUpdatePlanet2 .= "`crystal` = '".$WorkingPlanet['crystal']."', ";
-              $QryUpdatePlanet2 .= "`deuterium` = '".$WorkingPlanet['deuterium']."' ";
-              $QryUpdatePlanet2 .= "WHERE ";
-              $QryUpdatePlanet2 .= "`id` = '".$WorkingPlanet['id']."';";
-              doquery( $QryUpdatePlanet2);
+              doquery("UPDATE {{planets}} SET `b_tech_id` = '{$WorkingPlanet['b_tech_id']}', `b_tech` = '{$WorkingPlanet['b_tech']}',
+                `metal` = `metal` + {$costs['metal']}, `crystal` = `crystal` + '{$costs['crystal']}', `deuterium` = `deuterium` + '{$costs['deuterium']}' 
+                WHERE `id` = '{$WorkingPlanet['id']}' LIMIT 1;");
             }
-            break;
+          break;
+          
           case 'search':
-//            if ( !($InResearch && eco_unit_busy($CurrentUser, $CurrentPlanet, false, $Techno)) )
+//            if ( !($InResearch && eco_unit_busy($CurrentUser, $planet, false, $Techno)) )
             if ( !($InResearch || eco_lab_is_building($config, $que)) )
             {
-              if ( eco_can_build_unit($CurrentUser, $WorkingPlanet, $Techno) && IsElementBuyable($CurrentUser, $WorkingPlanet, $Techno) ) {
+              if ( eco_can_build_unit($CurrentUser, $WorkingPlanet, $Techno) && IsElementBuyable($CurrentUser, $WorkingPlanet, $Techno) ) 
+              {
                 $costs                        = GetBuildingPrice($CurrentUser, $WorkingPlanet, $Techno);
                 $WorkingPlanet['metal']      -= $costs['metal'];
                 $WorkingPlanet['crystal']    -= $costs['crystal'];
@@ -179,15 +90,9 @@ function ResearchBuildingPage (&$CurrentPlanet, $CurrentUser, $InResearch, $TheP
                 $UpdateData                   = true;
                 $InResearch                   = true;
 
-                $QryUpdatePlanet3  = "UPDATE {{planets}} SET ";
-                $QryUpdatePlanet3 .= "`b_tech_id` = '".$WorkingPlanet['b_tech_id']."', ";
-                $QryUpdatePlanet3 .= "`b_tech` = '".$WorkingPlanet['b_tech']."', ";
-                $QryUpdatePlanet3 .= "`metal` = '".$WorkingPlanet['metal']."', ";
-                $QryUpdatePlanet3 .= "`crystal` = '".$WorkingPlanet['crystal']."', ";
-                $QryUpdatePlanet3 .= "`deuterium` = '".$WorkingPlanet['deuterium']."' ";
-                $QryUpdatePlanet3 .= "WHERE ";
-                $QryUpdatePlanet3 .= "`id` = '".$WorkingPlanet['id']."';";
-                doquery( $QryUpdatePlanet3);
+                doquery("UPDATE {{planets}} SET `b_tech_id` = '{$WorkingPlanet['b_tech_id']}', `b_tech` = '{$WorkingPlanet['b_tech']}', 
+                  `metal` = `metal` - {$costs['metal']}, `crystal` = `crystal` - '{$costs['crystal']}', `deuterium` = `deuterium` - '{$costs['deuterium']}' 
+                  WHERE `id` = '{$WorkingPlanet['id']}' LIMIT 1;");
               }
             }
             elseif($InResearch)
@@ -198,117 +103,117 @@ function ResearchBuildingPage (&$CurrentPlanet, $CurrentUser, $InResearch, $TheP
             {
               $NoResearchMessage = $lang['labo_on_update'];
             };
-            break;
+          break;
         }
-        if ($UpdateData == true) {
-          $QryUpdateUser  = "UPDATE {{users}} SET ";
-          $QryUpdateUser .= "`b_tech_planet` = '".$CurrentUser['b_tech_planet']."' ";
-          $QryUpdateUser .= "WHERE ";
-          $QryUpdateUser .= "`id` = '".$CurrentUser['id']."';";
-          doquery( $QryUpdateUser);
+        if($UpdateData == true)
+        {
+          doquery("UPDATE {{users}} SET `b_tech_planet` = '{$CurrentUser['b_tech_planet']}' WHERE `id` = '{$CurrentUser['id']}';");
         }
         //byo; FIXed by PekopT 05.08.2008 thread   http://forum.ragezone.com/showthread.php?p=3734880#post3734880
-        $CurrentPlanet = $WorkingPlanet;
+        $planet = $WorkingPlanet;
         if ( is_array ($ThePlanet) ) {
           $ThePlanet     = $WorkingPlanet;
         } else {
-          $CurrentPlanet = $WorkingPlanet;
+          $planet = $WorkingPlanet;
           if ($TheCommand == 'search') {
-            $ThePlanet = $CurrentPlanet;
+            $ThePlanet = $planet;
           }
         }
       }
-    } else {
-      $bContinue = false;
     }
   }
 
-  $TechRowTPL = gettemplate('buildings_research_row');
-  $TechScrTPL = gettemplate('buildings_research_script');
+  $template = gettemplate('buildings_research', true);
 
-//  foreach($lang['tech'] as $Tech => $TechName)
   foreach($sn_data_group_tech as $Tech)
   {
-    if ( eco_can_build_unit($CurrentUser, $CurrentPlanet, $Tech))
+    if(!eco_can_build_unit($CurrentUser, $planet, $Tech))
     {
-      $RowParse                = $lang;
-      $RowParse['dpath']       = $dpath;
-      $RowParse['tech_id']     = $Tech;
-      $building_level          = $CurrentUser[$resource[$Tech]];
-      $RowParse['tech_level']  = ($building_level == 0) ? "" : "( ". $lang['level']. " ".$building_level." )";
-      $RowParse['tech_name']   = $lang['tech'][$Tech];
-      $RowParse['tech_descr']  = $lang['info'][$Tech]['description_short'];
-      $RowParse['tech_price']  = GetElementPrice($CurrentUser, $CurrentPlanet, $Tech);
-      $SearchTime              = GetBuildingTime($CurrentUser, $CurrentPlanet, $Tech);
-      $RowParse['search_time'] = ShowBuildTime($SearchTime);
-      $RowParse['tech_restp']  = $lang['Rest_ress'] ." ". GetRestPrice ($CurrentUser, $CurrentPlanet, $Tech, true);
-      $CanBeDone               = IsElementBuyable($CurrentUser, $CurrentPlanet, $Tech);
-
-      // Arbre de decision de ce que l'on met dans la derniere case de la ligne
-      if (!$InResearch) {
-        $LevelToDo = 1 + $CurrentUser[$resource[$Tech]];
-        if ($CanBeDone) {
-          if (eco_lab_is_building ( $config, $que )) {
-            // Le laboratoire est cours de construction ou d'evolution
-            // Et dans la config du systeme, on ne permet pas la recherche pendant
-            // que le labo est en construction ou evolution !
-            if ($LevelToDo == 1) {
-              $TechnoLink  = "<font color=#FF0000>". $lang['Rechercher'] ."</font>";
-            } else {
-              $TechnoLink  = "<font color=#FF0000>". $lang['Rechercher'] ."<br>".$lang['level']." ".$LevelToDo."</font>";
-            }
-          } else {
-            $TechnoLink  = "<a href=\"buildings.php?mode=research&cmd=search&tech=".$Tech."\">";
-            if ($LevelToDo == 1) {
-              $TechnoLink .= "<font color=#00FF00>". $lang['Rechercher'] ."</font>";
-            } else {
-              $TechnoLink .= "<font color=#00FF00>". $lang['Rechercher'] ."<br>".$lang['level']." ".$LevelToDo."</font>";
-            }
-            $TechnoLink  .= "</a>";
-          }
-        } else {
-          if ($LevelToDo == 1) {
-            $TechnoLink  = "<font color=#FF0000>". $lang['Rechercher'] ."</font>";
-          } else {
-            $TechnoLink  = "<font color=#FF0000>". $lang['Rechercher'] ."<br>".$lang['level']." ".$LevelToDo."</font>";
-          }
-        }
-
-      } else {
-        // Y a une construction en cours
-        if ($ThePlanet["b_tech_id"] == $Tech) {
-          // C'est le technologie en cours de recherche
-          $bloc       = $lang;
-          if ($ThePlanet['id'] != $CurrentPlanet['id']) {
-            // Ca se passe sur une autre planete
-            $bloc['tech_time']  = $ThePlanet["b_tech"] - time();
-            $bloc['tech_name']  = $lang['on'] ."<br>". $ThePlanet["name"];
-            $bloc['tech_home']  = $ThePlanet["id"];
-            $bloc['tech_id']    = $ThePlanet["b_tech_id"];
-          } else {
-            // Ca se passe sur la planete actuelle
-            $bloc['tech_time']  = $CurrentPlanet["b_tech"] - time();
-            $bloc['tech_name']  = "";
-            $bloc['tech_home']  = $CurrentPlanet["id"];
-            $bloc['tech_id']    = $CurrentPlanet["b_tech_id"];
-          }
-          $TechnoLink  = parsetemplate($TechScrTPL, $bloc);
-        } else {
-          // Technologie pas en cours recherche
-          $TechnoLink  = "<center>-</center>";
-        }
-      }
-      $RowParse['tech_link']  = $TechnoLink;
-      $TechnoList            .= parsetemplate($TechRowTPL, $RowParse);
+      continue;
     }
+
+    $building_level          = $CurrentUser[$resource[$Tech]];
+    $build_data = eco_get_build_data($CurrentUser, $planet, $Tech, $building_level);
+
+    // Arbre de decision de ce que l'on met dans la derniere case de la ligne
+    if($InResearch)
+    {
+      // Y a une construction en cours
+      if($ThePlanet['b_tech_id'] == $Tech)
+      {
+        // C'est le technologie en cours de recherche
+        $bloc       = $lang;
+        if ($ThePlanet['id'] != $planet['id'])
+        {
+          $build_planet = $ThePlanet;
+          $bloc['tech_name']  = "{$lang['on']}<br />{$build_planet['name']}";
+        }
+        else
+        {
+          $build_planet = $planet;
+          $bloc['tech_name']  = '';
+        }
+        $bloc['tech_time']  = $build_planet['b_tech'] - time();
+        $bloc['tech_home']  = $build_planet['id'];
+        $bloc['tech_id']    = $build_planet['b_tech_id'];
+        $TechnoLink  = parsetemplate(gettemplate('buildings_research_script'), $bloc);
+      }
+      else
+      {
+        // Technologie pas en cours recherche
+        $TechnoLink  = "<center>-</center>";
+      }
+    }
+    else
+    {
+      $LevelToDo = $building_level + 1;
+      $TechnoLink = $lang['Rechercher'] . ($LevelToDo == 1 ? '' : "<br>{$lang['level']}&nbsp;{$LevelToDo}");
+      if (IsElementBuyable($CurrentUser, $planet, $Tech) && !eco_lab_is_building($config, $que))
+      {
+        $TechnoLink  = "<a href=\"buildings.php?mode=research&cmd=search&tech={$Tech}\"><span class=\"success\">{$TechnoLink}</span></a>";
+      } else {
+        $TechnoLink  = "<span class=\"error\">{$TechnoLink}</span>";
+      }
+    }
+
+    $temp[RES_METAL]     = floor($planet['metal'] - $build_data[BUILD_CREATE][RES_METAL]); // + $fleet_list['own']['total'][RES_METAL]
+    $temp[RES_CRYSTAL]   = floor($planet['crystal'] - $build_data[BUILD_CREATE][RES_CRYSTAL]); // + $fleet_list['own']['total'][RES_CRYSTAL]
+    $temp[RES_DEUTERIUM] = floor($planet['deuterium'] - $build_data[BUILD_CREATE][RES_DEUTERIUM]); // + $fleet_list['own']['total'][RES_DEUTERIUM]
+
+    $template->assign_block_vars('production', array(
+      'ID'                => $Tech,
+      'NAME'              => $lang['tech'][$Tech],
+      'LEVEL'             => $building_level,
+      'DESCRIPTION'       => $lang['info'][$Tech]['description_short'],
+
+     'BUILD_CAN'          => $build_data['CAN'][BUILD_CREATE],
+     'TIME'               => pretty_time($build_data[BUILD_CREATE][RES_TIME]),
+     'METAL'              => $build_data[BUILD_CREATE][RES_METAL],
+     'CRYSTAL'            => $build_data[BUILD_CREATE][RES_CRYSTAL],
+     'DEUTERIUM'          => $build_data[BUILD_CREATE][RES_DEUTERIUM],
+     'ENERGY'             => $build_data[BUILD_CREATE][RES_ENERGY],
+                          
+     'METAL_PRINT'        => pretty_number($build_data[BUILD_CREATE][RES_METAL], true, $planet['metal']),
+     'CRYSTAL_PRINT'      => pretty_number($build_data[BUILD_CREATE][RES_CRYSTAL], true, $planet['crystal']),
+     'DEUTERIUM_PRINT'    => pretty_number($build_data[BUILD_CREATE][RES_DEUTERIUM], true, $planet['deuterium']),
+     'ENERGY_PRINT'       => pretty_number($build_data[BUILD_CREATE][RES_ENERGY], true, $planet['energy_max']),
+                          
+     'METAL_REST'         => pretty_number($temp[RES_METAL], true, true),
+     'CRYSTAL_REST'       => pretty_number($temp[RES_CRYSTAL], true, true),
+     'DEUTERIUM_REST'     => pretty_number($temp[RES_DEUTERIUM], true, true),
+     'METAL_REST_NUM'     => $temp[RES_METAL],
+     'CRYSTAL_REST_NUM'   => $temp[RES_CRYSTAL],
+     'DEUTERIUM_REST_NUM' => $temp[RES_DEUTERIUM],
+
+     'TECH_LINK'          => $TechnoLink,
+    ));
   }
 
-  $PageParse                = $lang;
-  $PageParse['noresearch']  = $NoResearchMessage;
-  $PageParse['technolist']  = $TechnoList;
-  $Page                    .= parsetemplate(gettemplate('buildings_research'), $PageParse);
+  $template->assign_vars(array(
+    'MESSAGE'            => $NoResearchMessage,
+  ));
 
-  display( $Page, $lang['Research'] );
+  display(parsetemplate($template), $lang['Research']);
 }
 
 // History revision
