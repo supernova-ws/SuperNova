597ac75454ad826429efd0d1f9f5298d13138d52
Gorlum <gorlum@supernova.ws>
2011-12-21 19:40:21 +0200

    fix-ECO: Fixed error with structures que introduced with last commit

--- includes/constants.php
+++ includes/constants.php
@@ -16,7 +16,7 @@ if(!defined('INSIDE'))
 
 define('DB_VERSION', '31');
 define('SN_RELEASE', '32');
-define('SN_VERSION', '32a14');
+define('SN_VERSION', '32a15');
 
 // Game type constants starts with GAME_
 define('GAME_SUPERNOVA', 0);
