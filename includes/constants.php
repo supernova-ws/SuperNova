e5074dfc3e188aa925f4cc53f1c0efec2120656a
Gorlum <gorlum@supernova.ws>
2012-03-28 23:46:16 +0300

    INT-misc-34a11.3: Expedition navbar widget now leads to send fleet page

--- includes/constants.php
+++ includes/constants.php
@@ -16,7 +16,7 @@ if(!defined('INSIDE'))
 
 define('DB_VERSION', '33');
 define('SN_RELEASE', '34');
-define('SN_VERSION', '34a11.2');
+define('SN_VERSION', '34a11.3');
 
 define('SN_RELEASE_STABLE', '33d0'); // Latest stable release
 
