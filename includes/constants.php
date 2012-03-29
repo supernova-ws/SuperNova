08e3d03327a527183dbc1f0be61c1256509f61ff
Gorlum <gorlum@supernova.ws>
2012-03-30 02:46:26 +0300

    INT-fix-34a11.9: Added CR on mercenary page for error messages per mercenary

--- includes/constants.php
+++ includes/constants.php
@@ -16,7 +16,7 @@ if(!defined('INSIDE'))
 
 define('DB_VERSION', '33');
 define('SN_RELEASE', '34');
-define('SN_VERSION', '34a11.8');
+define('SN_VERSION', '34a11.9');
 
 define('SN_RELEASE_STABLE', '33d0'); // Latest stable release
 
