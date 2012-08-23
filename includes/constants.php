ea0713d94161e98816b756b4281c9a717b295d5f
Gorlum <gorlum@supernova.ws>
2012-08-23 21:30:32 +0300

    FLT-fix-35a9.21: Fixed error sending ship if it's ID greater then 300 or less then 200

--- includes/constants.php
+++ includes/constants.php
@@ -31,7 +31,7 @@ define('SHOW_ADMIN', 1);
 
 define('DB_VERSION', '34');
 define('SN_RELEASE', '35');
-define('SN_VERSION', '35a9.20');
+define('SN_VERSION', '35a9.21');
 define('SN_RELEASE_STABLE', '34d0'); // Latest stable release
 
 // Game type constants starts with GAME_
