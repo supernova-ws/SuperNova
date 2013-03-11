f599b1342265a5719a1321550ca2d3db9d0f7837
Gorlum <gorlum@supernova.ws>
2013-03-12 00:47:01 +0200

    SYS-37a3.15: Changed time measurement algorythm to support some weirdo server configurations

--- time_probe.php
+++ time_probe.php
@@ -10,7 +10,8 @@ $time_diff   = $time_local  - $time_server
 $time_server = $time_local  - $time_diff
 */
 
-  $time_diff = ($time_local = intval($_POST['localtime'] / 1000)) ? $time_local - $time_now : 0;
+//  $time_diff = ($time_local = intval($_POST['localtime'] / 1000)) ? $time_local - $time_now : 0;
+  $time_diff = ($time_local = intval($_POST['localtime'] / 1000)) ? $time_local - $time_now + intval($_POST['utcoffset']) - date('Z') : 0;
   if($user['id'] && !$user['user_time_diff_forced'])
   {
     doquery("UPDATE {{users}} SET `user_time_diff` = {$time_diff} WHERE `id` = {$user['id']} LIMIT 1;");
