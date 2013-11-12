1572f60383fda90e84366757d2af6d05c33ac013
Gorlum <gorlum@supernova.ws>
2013-11-13 01:13:03 +0200

    ECO-misc-38a2.1: Added new files for Metamatter

---
+++ language/en/payment.mo.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+#############################################################################
+#  Filename: payment.mo.php
+#  Project: SuperNova.WS
+#  Website: http://www.supernova.ws
+#  Description: Massive Multiplayer Online Browser Space Startegy Game
+#
+#  Copyright © 2009 Gorlum for Project "SuperNova.WS"
+#############################################################################
+*/
+
+/**
+*
+* @package language
+* @system [English]
+* @version 38a2.1
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+
+if (!defined('INSIDE')) die();
+
+$lang = array_merge($lang, array(
+  // Metamatter
+  'sys_metamatter_what_header' => 'What is Metamatter',
+  'sys_metamatter_what_description' => 'Метаматерия - это весьма условное название для особого состояния Вселенной. Фактически - это даже не материя, а факторизируемая вероятность.<br />
+  У Метаматерии нет состояния - и в то же время она находится во всех состояних. Метаматерия нигде не находится - и в то же время находится везде. Потенциально метаматерия может стать чем угодно и где угодно - если правильно актуализировать вероятность.<br />
+  Увы, свойства Метаматерии и методы её актуализации еще исследованы слабо. Поэтому на сегодня доступен весьма узкий спектр применения ММ:<ul>
+  <li>Метаматерию можно сконвертировать в Тёмную Материю по курсу 1 ММ = 1 ТМ без дополнительных затрат</li>
+  </ul>',
+
+  'pay_mm_convert_header' => 'Конвертация Метаматерии в Тёмную Материю',
+  'pay_mm_convert_text' => 'Метаматерия конвертируется в Тёмную Материю по курсу 1 к 1',
+  'pay_mm_convert_no_mm' => 'Нет Метаматерии - купите её сначала',
+  'pay_mm_convert_prefix' => 'Единицы Метаматерии',
+  'pay_mm_convert_suffix' => '',
+  'pay_mm_convert_do' => 'Сконвертировать в ТМ',
+
+  'pay_msg_mm_convert_wrong_amount' => 'Неправильное количество Метаматерии',
+  'pay_msg_mm_convert_not_enough' => 'Не хватает Метаматерии для конвертации в Тёмную Материю',
+  'pay_msg_mm_convert_mm_error' => 'Ошибка изменения количеста Метаматерии',
+  'pay_msg_mm_convert_dm_error' => 'Ошибка изменения количеста Тёмной Материи',
+
+  'pay_mm_buy' => 'Purchase Metamatter',
+  'pay_mm_buy_text_cost' => 'Price for',
+  'pay_mm_buy_text_unit' => 'is',
+  'pay_mm_buy_url_description' => 'In addition you can purchase for real money',
+  'pay_mm_buy_url_get'  => 'Click here to read details',
+  'pay_mm_buy_url_none' => 'Contact with server Administration to get Metamatter',
+
+
+  'pay_mm_bonus_header' => 'Metamatter cost and bonuses for bulk purchases',
+  'pay_mm_bonus' => 'When you purchasing large amounts of Metamatter you recieve bonuses:',
+  'pay_mm_bonus_each' => 'from %s MM - %d%% bonus to purchased MM amount',
+
+
+  'pay_mm_buy_step1_text' => 'Select amount of MM you wish to purchase, select payment system and confirm your selection',
+  'pay_mm_buy_unit' => 'Metamatter units',
+  'pay_mm_buy_select' => 'Payment system',
+  'pay_mm_buy_confirm' => 'Confirm selection',
+  'pay_mm_buy_payment_selected' => 'Purchase would be made using payment system',
+
+  'pay_mm_buy_step2_text' => 'Verify selected amount of Metamatter and selected payment system. If everything is OK press button "Purchase Metamatter". If there is any error - press button "Discard and start again"',
+  'pay_mm_buy_pay' => 'Purchase Metamatter',
+  'pay_mm_buy_reset' => 'Discard and start again',
+  'pay_mm_buy_in_progress' => 'Payment in progress...',
+  'pay_mm_buy_conversion_cost' => 'Cost of %s Metamatter will be %s %s',
+
+  'pay_currency_name' => 'Currency',
+  'pay_currency_symbol' => 'Symbol',
+  'pay_currency_choose' => 'Choose payment currency',
+  'pay_currency_list' => array(
+    'RUB' => 'Russian ruble',
+    'USD' => 'Dollar USA',
+    'EUR' => 'Euro',
+    'UAH' => 'Ukrainian hryvna',
+    'WMR' => 'WebMoney rouble',
+    'WMZ' => 'WebMoney dollar',
+    'WME' => 'WebMoney euro',
+    'WMU' => 'WebMoney hryvna',
+  ),
+
+  'pay_currency_exchange_title' => 'Internal currency exchange',
+  'pay_currency_exchange_direct' => 'Exchange rate',
+  'pay_currency_exchange_reverse' => 'Reverse rate',
+  'pay_currency_exchange_mm' => 'MM for 1 currency',
+  'pay_currency_exchange_note' => 'Internal exchange rates used to calculate payment amount in payment system currency. Exchange rates does not includes commission of payment system(s)',
+
+  'pay_msg_mm_purchase_complete'   => 'You succesfully paid for %d Metamatter via %s. You gained %s Metamatter',
+  'pay_msg_mm_purchase_incomplete' => 'You payment for %d Metamatter via %s currently in progress. If you feel it wrong please contact Administration',
+  'pay_msg_mm_purchase_test'       => 'Really you did not gain anything! Because it was a test payment ha-ha-ha! If you feel it wrong - contact Administration',
+
+  'pay_msg_request_user_found' => 'User found',
+
+  'pay_msg_request_unsupported' => 'Unsupported request',
+  'pay_msg_request_signature_invalid' => 'Wrong request signature',
+  'pay_msg_request_user_invalid' => 'User ID is invalid',
+  'pay_msg_request_server_wrong' => 'Wrong server',
+  'pay_msg_request_payment_amount_invalid' => 'Wrong payment amount',
+  'pay_msg_request_payment_id_invalid' => 'Wrong payment ID',
+  'pay_msg_request_payment_date_invalid' => 'Wrong payment date',
+  'pay_msg_request_internal_error' => 'Server internal error. Try again later',
+  'pay_msg_request_paylink_unsupported' => 'This type of paylink is not supported. It\'s looks like you using outdated version of SuperNova which incompatible with selected payment module',
+  'pay_msg_request_payment_write_error' => 'Payment write error',
+
+  'pay_msg_module_disabled' => 'Payment module disabled',
+
+  'pay_msg_mm_request_amount_invalid' => 'Wrong Metamatter amount',
+  'pay_msg_mm_request_config_invalid' => 'There is error in payment module configuration. Please contact server AMMinistration',
+  'pay_msg_mm_request_mm_adjust_error' => 'Error adjusting Metamatter',
+
+));
---
+++ metamatter.php
@@ -0,0 +1,251 @@
+<?php
+
+// Придумать какой статус должен быть у глобальных ответов, что бы не перекрывать статусы платежных систем
+// Может добавить спецстатус "Ответ системы платежа" и парсить дальше getMessage
+// см constants.php
+
+include_once('common.' . substr(strrchr(__FILE__, '.'), 1));
+
+if(!sn_module_get_active_count('payment'))
+{
+  sys_redirect('overview.php');
+  die();
+}
+
+lng_include('payment');
+
+$template = gettemplate('metamatter', true);
+
+if(sys_get_param('mm_convert_do'))
+{
+  try
+  {
+    if(!($mm_convert = sys_get_param_id('mm_convert')))
+    {
+      throw new exception($lang['pay_msg_mm_convert_wrong_amount'], ERR_ERROR);
+    }
+
+    sn_db_transaction_start();
+    $user = doquery("SELECT * FROM {{users}} WHERE `id` = {$user['id']} FOR UPDATE", true);
+    if($mm_convert > mrc_get_level($user, '', RES_METAMATTER))
+    {
+      throw new exception($lang['pay_msg_mm_convert_not_enough'], ERR_ERROR);
+    }
+
+    $payment_comment = sprintf("Игрок сконвертировал %d Метаматерии в Тёмную Материю", $mm_convert);
+
+    if(!mm_points_change($user['id'], RPG_CONVERT_MM, -$mm_convert, $payment_comment))
+    {
+      throw new exception($lang['pay_msg_mm_convert_mm_error'], ERR_ERROR);
+    }
+    if(!rpg_points_change($user['id'], RPG_CONVERT_MM, $mm_convert, $payment_comment))
+    {
+      throw new exception($lang['pay_msg_mm_convert_dm_error'], ERR_ERROR);
+    }
+
+    $template->assign_block_vars('result', array(
+      'STATUS'  => ERR_NONE,
+      'MESSAGE' => sprintf('Конвертация %1$s единиц Метаматерии в %1$s единиц Тёмной Материи успешно произведена', pretty_number($mm_convert)),
+    ));
+
+    sn_db_transaction_commit();
+  }
+  catch(exception $e)
+  {
+    sn_db_transaction_rollback();
+    $template->assign_block_vars('result', $response = array(
+      'STATUS'  => $e->getCode(),
+      'MESSAGE' => $e->getMessage(),
+    ));
+  }
+}
+
+if(isset(sn_module_payment::$bonus_table) && is_array(sn_module_payment::$bonus_table))
+{
+  foreach(sn_module_payment::$bonus_table as $sum => $discount)
+  {
+    if($discount)
+    {
+      $template->assign_block_vars('discount', array(
+        'SUM' => $sum,
+        'DISCOUNT' => $discount * 100,
+        'TEXT' => sprintf($lang['pay_mm_bonus_each'], pretty_number($sum), $discount * 100),
+      ));
+    }
+  }
+}
+
+if($payment_id = sys_get_param_id('payment_id'))
+{
+  $payment = doquery("SELECT * FROM {{payment}} WHERE `payment_id` = {$payment_id} LIMIT 1;", true);
+  if($payment && $payment['payment_user_id'] == $user['id'])
+  {
+    if($payment['payment_status'] == PAYMENT_STATUS_COMPLETE || $payment['payment_status'] == PAYMENT_STATUS_TEST)
+    {
+      $template->assign_block_vars('result', array('MESSAGE' => sprintf($lang['pay_msg_mm_purchase_complete'], $payment['payment_dark_matter_paid'], $payment['payment_module_name'], $payment['payment_dark_matter_gained'])));
+    }
+    if($payment['payment_status'] == PAYMENT_STATUS_NONE)
+    {
+      $template->assign_block_vars('result', array(
+        'MESSAGE' => sprintf($lang['pay_msg_mm_purchase_incomplete'], $payment['payment_dark_matter_paid'], $payment['payment_module_name']),
+        'STATUS' => 1,
+      ));
+    }
+    if($payment['payment_status'] == PAYMENT_STATUS_TEST)
+    {
+      $template->assign_block_vars('result', array(
+        'MESSAGE' => sprintf($lang['pay_msg_mm_purchase_test']),
+        'STATUS' => -1,
+      ));
+    }
+  }
+}
+
+$unit_available_amount_list = &sn_module_payment::$bonus_table;
+
+$request = array(
+  'metamatter' => sys_get_param_float('metamatter'),
+);
+$request['metamatter'] = isset($unit_available_amount_list[$request['metamatter']]) ? $request['metamatter'] : 0;
+if(!$request['metamatter'])
+{
+  unset($_POST);
+}
+
+$payment_module_valid = false;
+$payment_module = sys_get_param_str('payment_module');
+foreach($sn_module_list['payment'] as $module_name => $module)
+{
+  if(!is_object($module) || !$module->manifest['active'])
+  {
+    continue;
+  }
+
+  lng_include($module_name, $module->manifest['root_relative']);
+
+  $template->assign_block_vars('payment_module', array(
+    'ID' => $module_name,
+    'NAME' => $lang["module_{$module_name}_name"],
+    'DESCRIPTION' => $lang["module_{$module_name}_description"],
+  ));
+
+  $payment_module_valid = $payment_module_valid || $module_name == $payment_module;
+}
+// If payment_module invalid - making it empty OR if there is only one payment_module - selecting it
+$payment_module = $payment_module_valid ? $payment_module : (count($sn_module_list['payment']) == 1 ? $module_name : '');
+
+foreach($lang['pay_currency_list'] as $key => $value)
+{
+  $var_name = 'payment_currency_exchange_' . strtolower($key);
+  $course = $config->$var_name;
+  if(!$course || $key == $config->payment_currency_default)
+  {
+    continue;
+  }
+  $template->assign_block_vars('exchange', array(
+    'SYMBOL' => $key,
+    'TEXT' => $value,
+    'COURSE_DIRECT' => pretty_number($course, 4),
+    'COURSE_REVERSE' => pretty_number(1 / $course, 4),
+    'MM_PER_CURRENCY' => pretty_number(sn_module_payment::currency_convert(1, $key, 'MM_')),
+    // 'UNIT_PER_LOT' => sn_module_payment::currency_convert(2500, 'MM_', $key),
+  ));
+}
+
+if($request['metamatter'] && $payment_module)
+{
+  try
+  {
+    // Any possible errors about generating paylink should be raised in module!
+    $pay_link = $sn_module[$payment_module]->compile_request($request);
+
+    // Поддержка дополнительной информации
+    if(is_array($pay_link['RENDER']))
+    {
+      foreach($pay_link['RENDER'] as $html_data)
+      {
+        $template->assign_block_vars('render', $html_data);
+        if(isset($html_data['VALUE']) && is_array($html_data['VALUE']))
+        {
+          foreach($html_data['VALUE'] as $value_id => $value_value)
+          {
+            $template->assign_block_vars('render.value', array(
+              'FIELD' => $value_id,
+              'VALUE' => $value_value,
+            ));
+          }
+        }
+      }
+    }
+
+    // Поддержка передачи данных для многошаговых платежных систем
+    if(is_array($pay_link['DATA']))
+    {
+      foreach($pay_link['DATA'] as $key => $value)
+      {
+        $template->assign_block_vars('pay_link_data', array(
+          'FIELD' => $key,
+          'VALUE' => $value,
+        ));
+      }
+    }
+
+    /* // TODO PRESUMABLY OUTDATED
+    if(is_string($pay_link) && strpos($pay_link, 'http') === 0)
+    {
+      $template->assign_vars(array(
+        'PAY_LINK' => $pay_link,
+        'PAY_LINK_METHOD' => 'LINK',
+      ));
+    }
+    else
+    */
+
+    if(is_array($pay_link) && in_array($pay_link['PAY_LINK_METHOD'], array('POST', 'GET', 'LINK', 'STEP')))
+    { // TODO Переделать это под assign_vars_recursive и возвращать пустые строки если нет платежного метода - для унификации формы в темплейте
+      $template->assign_vars(array(
+        'PAY_LINK_METHOD' => $pay_link['PAY_LINK_METHOD'],
+        'PAY_LINK_URL' => $pay_link['PAY_LINK_URL'],
+      ));
+    }
+    else
+    {
+      throw new exception($lang['pay_msg_request_paylink_unsupported'], ERR_ERROR);
+    }
+  }
+  catch(exception $e)
+  {
+    $template->assign_block_vars('result', $response = array(
+      'STATUS'  => $e->getCode(),
+      'MESSAGE' => $e->getMessage(),
+    ));
+  }
+}
+
+foreach($unit_available_amount_list as $unit_amount => $discount)
+{
+  $template->assign_block_vars('mm_amount', array(
+    'VALUE' => $unit_amount,
+    'TEXT' => pretty_number($unit_amount),
+  ));
+}
+
+$template->assign_vars(array(
+  'PAGE_HEADER' => $lang['sys_metamatter'],
+
+  'URL_PURCHASE' => $config->url_purchase_metamatter,
+
+  'PAYMENT_MODULE' => $payment_module,
+  'PAYMENT_MODULE_NAME' => $lang["module_{$payment_module}_name"],
+  'PAYMENT_MODULE_DESCRIPTION' => $lang["module_{$payment_module}_description"],
+
+  'UNIT_AMOUNT' => (float)$request['metamatter'],
+  'UNIT_AMOUNT_TEXT' => pretty_number($request['metamatter'], true, true),
+
+  'PAYMENT_CURRENCY_EXCHANGE_DEFAULT' => pretty_number($config->payment_currency_exchange_mm_, true, true),
+  'PAYMENT_CURRENCY_DEFAULT_TEXT' => $lang['pay_currency_list'][$config->payment_currency_default],
+
+  'METAMATTER' => mrc_get_level($user, '', RES_METAMATTER),
+));
+
+display($template, $lang['sys_metamatter']);
