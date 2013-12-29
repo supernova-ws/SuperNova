1d4c58b28ada7a4512e1aee9e71dc0c8fd6ffaae
Gorlum <gorlum@supernova.ws>
2013-12-29 15:31:57 +0200

    DOC-38a7.1: changelog editing

--- includes/constants.php
+++ includes/constants.php
@@ -31,7 +31,7 @@ define('SHOW_ADMIN', 1);
 
 define('DB_VERSION', '37');
 define('SN_RELEASE', '37');
-define('SN_VERSION', '38a7.0');
+define('SN_VERSION', '38a7.1');
 define('SN_RELEASE_STABLE', '37d1'); // Latest stable release
 
 // Game type constants starts with GAME_
