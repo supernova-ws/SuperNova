af2dd88106bb27a9d2ca92fda759378250420e7c
Gorlum <gorlum@supernova.ws>
2014-11-07 07:15:32 +0200

    CHAT-misc-39a16.8: Support for new nick renderer

--- includes/pages/chat.php
+++ includes/pages/chat.php
@@ -89,7 +89,8 @@ function sn_chat_add_model()
   if(($message = sys_get_param_str('message')) && $user['username'])
   {
     $ally_id = sys_get_param('ally') && $user['ally_id'] ? $user['ally_id'] : 0;
-    $nick = mysql_real_escape_string(player_nick_render_to_html($user, array('color' => true, 'icons' => true, 'ally' => !$ally_id)));
+    $nick = mysql_real_escape_string(player_nick_compact(player_nick_render_current_to_array($user, array('color' => true, 'icons' => true, 'ally' => !$ally_id))));
+
     $message = preg_replace("#(?:https?\:\/\/(?:.+)?\/index\.php\?page\=battle_report\&cypher\=([0-9a-zA-Z]{32}))#", "[ube=$1]", $message);
 
     doquery("INSERT INTO {{chat}} (chat_message_sender_id, user, ally_id, message, timestamp) VALUES ('{$user['id']}', '{$nick}', '{$ally_id}', '{$message}', '{$time_now}');");
@@ -158,10 +159,11 @@ function sn_chat_msg_view($template = null)
       FROM
         {{chat}} AS c
         LEFT JOIN {{users}} AS u ON u.id = c.chat_message_sender_id
-      WHERE c.ally_id = '{$alliance}' {$where_add} ORDER BY messageid DESC LIMIT {$start_row}, {$page_limit};");
+      WHERE c.chat_message_recipient_id IS NULL AND c.ally_id = '{$alliance}' {$where_add} ORDER BY messageid DESC LIMIT {$start_row}, {$page_limit};");
     while($chat_row = mysql_fetch_assoc($query))
     {
       // Little magik here - to retain HTML codes from DB and stripping HTML codes from nick
+      $chat_row['user'] = player_nick_render_to_html($chat_row['user']);
       $nick_stripped = htmlentities(strip_tags($chat_row['user']), ENT_QUOTES, 'utf-8');
       $nick = str_replace(strip_tags($chat_row['user']), $nick_stripped, $chat_row['user']);
       if(!$history)
