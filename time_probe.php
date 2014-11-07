6203c1ee7240f785df9c4ae835a3f492ffe5f86c
Gorlum <gorlum@supernova.ws>
2014-11-07 05:57:06 +0200

    DOC-39a16.5: Changelog editing

--- time_probe.php
+++ time_probe.php
@@ -13,7 +13,7 @@ $time_server = $time_local  - $time_diff
 // $time_diff = ($time_local = intval($_POST['localtime'] / 1000)) ? $time_local - $time_now + intval($_POST['utcoffset']) - date('Z') : 0;
 
 if(isset($user['user_time_diff_forced']) && $user['user_time_diff_forced']) {
-  $time_diff = $user['user_time_diff'];
+  $time_diff = intval($user['user_time_diff']);
 } else {
   $time_diff = ($time_local = intval($_POST['localtime'] / 1000)) ? $time_local - SN_TIME_NOW : 0;
   $time_utcoffset = ($time_local_utcoffset = intval($_POST['utcoffset'])) ? $time_local_utcoffset - date('Z') : 0;
