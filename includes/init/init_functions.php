4dcc0f63cabc944c48f2b74b10776b0399838994
Gorlum <gorlum@supernova.ws>
2015-03-20 05:10:41 +0200

    DOC-39b8.6: Changelog editing

--- includes/init/init_functions.php
+++ includes/init/init_functions.php
@@ -1,36 +1,26 @@
 <?php
 
 // ------------------------------------------------------------------------------------------------------------------------------
-function sn_sys_load_php_files($dir_name, $phpEx = 'php', $modules = false)
-{
-  if(file_exists($dir_name))
-  {
+function sn_sys_load_php_files($dir_name, $phpEx = 'php', $modules = false) {
+  if(file_exists($dir_name)) {
     $dir = opendir($dir_name);
-    while(($file = readdir($dir)) !== false)
-    {
-      if($file == '..' || $file == '.')
-      {
+    while(($file = readdir($dir)) !== false) {
+      if($file == '..' || $file == '.') {
         continue;
       }
 
       $full_filename = $dir_name . $file;
-      if($modules && is_dir($full_filename))
-      {
-        if(file_exists($full_filename = "{$full_filename}/{$file}.{$phpEx}"))
-        {
+      if($modules && is_dir($full_filename)) {
+        if(file_exists($full_filename = "{$full_filename}/{$file}.{$phpEx}")) {
           require_once($full_filename);
           // Registering module
-          if(class_exists($file))
-          {
+          if(class_exists($file)) {
             new $file($full_filename);
           }
         }
-      }
-      else
-      {
+      } else {
         $extension = substr($full_filename, -strlen($phpEx));
-        if($extension == $phpEx)
-        {
+        if($extension == $phpEx) {
           require_once($full_filename);
         }
       }
@@ -38,8 +28,7 @@ function sn_sys_load_php_files($dir_name, $phpEx = 'php', $modules = false)
   }
 }
 
-function sys_refresh_tablelist($db_prefix)
-{
+function sys_refresh_tablelist($db_prefix) {
   global $sn_cache;
 
   $tl = array();
