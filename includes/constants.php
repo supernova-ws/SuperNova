ed6a2444a2269da1067a2eaf6526c35edf7ced6c
Gorlum <gorlum@supernova.ws>
2015-10-27 20:41:52 +0200

    FLEET-misc-40a18.15: Changed border on fleet-page-1 to thin ones

--- includes/constants.php
+++ includes/constants.php
@@ -4,7 +4,7 @@ defined('INSIDE') or die('Hacking attempt');
 
 define('DB_VERSION', '39');
 define('SN_RELEASE', '39');
-define('SN_VERSION', '40a18.13');
+define('SN_VERSION', '40a18.15');
 define('SN_RELEASE_STABLE', '39d2'); // Latest stable release
 
 // Game type constants starts with GAME_
