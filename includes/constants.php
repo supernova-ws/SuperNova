ab7f4d5c6029e2bdc140f52242041dcaafa099e6
Gorlum <gorlum@supernova.ws>
2015-12-18 00:05:35 +0200

    release-40d0: Project 'SuperNova.WS' Release 40

--- includes/constants.php
+++ includes/constants.php
@@ -4,7 +4,7 @@ defined('INSIDE') or die('Hacking attempt');
 
 define('DB_VERSION', '40');
 define('SN_RELEASE', '40');
-define('SN_VERSION', '40c1.3');
+define('SN_VERSION', '40d0');
 define('SN_RELEASE_STABLE', '39d2'); // Latest stable release
 
 // Game type constants starts with GAME_
