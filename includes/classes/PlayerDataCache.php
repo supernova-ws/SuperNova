0e3a25d5c5c3039f393b161f83653d461eaf88a7
Gorlum <gorlum@supernova.ws>
2016-01-27 20:56:36 +0200

    CODE-misc-41a3.5: Added placeholder for PlayerDataCache

---
+++ includes/classes/PlayerDataCache.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * User: Gorlum
+ * Date: 27.01.2016
+ * Time: 20:44
+ */
+class PlayerDataCache {
+  /**
+   * Кэш игры
+   *
+   * @var classCache $cache
+   */
+  protected static $cache;
+
+  /**
+   * @var int $player_id
+   */
+  protected $player_id;
+
+  public function __construct() {
+    $this->cache = classSupernova::$cache;
+  }
+
+  /**
+   * @param int $player_id
+   */
+  public function player_id_set($player_id) {
+
+  }
+
+}
