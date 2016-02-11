a6d948f9419302741fe13f24d3291640a16672c0
Gorlum <gorlum@supernova.ws>
2016-02-11 21:52:04 +0200

    CODE-misc-41a4.17: flt_planet_capture() usage added

--- includes/includes/flt_mission_attack.php
+++ includes/includes/flt_mission_attack.php
@@ -7,6 +7,7 @@ require_once('includes/includes/ube_attack_calculate.php');
 */
 
 
+// Used by game_skirmish
 function flt_planet_capture(&$fleet_row, &$combat_data) { return sn_function_call(__FUNCTION__, array(&$fleet_row, &$combat_data, &$result)); }
 
 function sn_flt_planet_capture(&$fleet_row, &$combat_data, &$result) {
