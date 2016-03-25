5197c7af92b665d79f9cced9a4c3c7e523dd2f14
Gorlum <gorlum@supernova.ws>
2016-03-25 21:28:28 +0200

    MOD-feat-41a6.37: Rewrote sn_module to reduce code smell

--- includes/classes/module.php
+++ includes/classes/module.php
@@ -2,36 +2,37 @@
 
 class sn_module {
   public $manifest = array(
-    'package' => 'core',
-    'name' => 'sn_module',
-    'version' => '1c0',
-    'copyright' => 'Project "SuperNova.WS" #41a6.35# copyright © 2009-2014 Gorlum',
+    'package'   => 'core',
+    'name'      => 'sn_module',
+    'version'   => '1c0',
+    'copyright' => 'Project "SuperNova.WS" #41a6.37# copyright © 2009-2014 Gorlum',
 
-//    'require' => null,
+    'require'       => array(),
     'root_relative' => '',
 
     'installed' => true,
-    'active' => true,
+    'active'    => true,
 
-    // 'constants' array - contents of this array would be instaled into engine
-    'constants' => array(
-//      'UNIT_STRUCTURE_NEW' => 999999,
-    ),
+    // 'constants' array - contents of this array would be instaled into engine. 'UNIT_STRUCTURE_NEW' => 999999,
+    'constants' => array(),
 
-    'vars' => array(), // Just a placeholder. vars assigned via special method __assign_vars(). Need 'cause of new constants that can be defined within module. See below
+    'vars'      => array(), // Just a placeholder. vars assigned via special method __assign_vars(). Need 'cause of new constants that can be defined within module. See below
 
-    // 'functions' array - this functions would be installed as hooks
-    // Key: overwritable function name to replace
-    // Value: which method to use. Format: [*][<object_name>][.]<method>
-    // '*' means that new function would replace old one
-    // If object_name is ommited but "." is present - hook linked to global function
-    // If only "method" present - overwritable linked to appropriate method of current object
-    // Function/Method should be accessible on module init
-    'functions' => array(
-//      'test_object_test_method' => 'test_object.test_method',
-//      'test_function' => '.my_test_function',
-//      'this_object_test_method' => 'test_method',
-    ),
+    /**
+     * 'functions' array - this functions would be installed as hooks
+     * Key: overwritable function name to replace
+     * Value: which method to use. Format: [*][<object_name>][.]<method>
+     * '*' means that new function would replace old one
+     * If object_name is ommited but "." is present - hook linked to global function
+     * If only "method" present - overwritable linked to appropriate method of current object
+     * Examples:
+     * 'test_object.test_method' - will
+     * Function/Method should be accessible on module init
+     * //      'test_object_test_method' => 'test_object.test_method',
+     * //      'test_function' => '.my_test_function',
+     * //      'this_object_test_method' => 'test_method',
+     */
+    'functions' => array(),
 
     // 'menu' array - this menu items would be merged into main game menu
     // Array element almost identical to $sn_menu with additional param 'LOCATION'.
@@ -41,10 +42,10 @@ class sn_module {
     // -/+ indicates that new item should be placed before/after identified menu item (or whole menu). If ommited and menu item exists - new item will replace previous one
     // Empty or non-existent LOCATION equivalent to '+' - place item at end of menu
     // Non-existent menu_item_id treated as ommited
-    'menu' => array(),
+    'menu'      => array(),
 
     // 'page' array - defines pages which will handle this module and appropriate handlers
-    'page' => array(),
+    'page'      => array(),
   );
 
   protected $config = array();
@@ -60,39 +61,16 @@ class sn_module {
    * @return array
    */
   function __assign_vars() {
-    return array(
-/*
-      'sn_data' => array(
-        UNIT_STRUCTURE_NEW => array( // Will honor new constants
-          'name' => 'robot_factory',
-          'type' => UNIT_STRUCTURE,
-          'location' => LOC_PLANET,
-          'cost' => array(
-            RES_METAL     => 400,
-            RES_CRYSTAL   => 120,
-            RES_DEUTERIUM => 200,
-            RES_ENERGY    => 0,
-            'factor' => 2,
-          ),
-          'metal' => 400,
-          'crystal' => 120,
-          'deuterium' => 200,
-          'energy' => 0,
-          'factor' => 2,
-        ),
-      ),
-*/
-    );
+    return array();
   }
 
   function loadModuleRootConfig() {
-    static $i;
-
     require SN_ROOT_PHYSICAL . 'config.php';
 
     $module_config_array = get_class($this) . '_config';
     if(!empty($$module_config_array) && is_array($$module_config_array)) {
       $this->config = $$module_config_array;
+
       return true;
     }
 
@@ -129,122 +107,157 @@ class sn_module {
     $sn_module[$class_module_name] = $this;
   }
 
-  protected function __patch_menu(&$sn_menu_extra, &$menu_patch) {
-    if(isset($menu_patch) && is_array($menu_patch) && !empty($menu_patch)) {
-      foreach($menu_patch as $menu_item_name => $menu_item_data) {
-        $sn_menu_extra[$menu_item_name] = $menu_item_data;
-      }
-    }
-  }
-
 
   public function initialize() {
+    global $sn_menu_extra, $sn_menu_admin_extra;
+
     // Checking module status - is it installed and active
     $this->check_status();
     if(!$this->manifest['active']) {
       return;
     }
 
+    $this->setSystemConstants();
+    $this->setSystemVariables();
+    $this->addSystemHandlers();
+    $this->mergeI18N();
+
+    // Patching game menu - if any
+    isset($this->manifest['menu']) && $this->mergeMenu($sn_menu_extra, $this->manifest['menu']);
+    isset($this->manifest['menu_admin']) && $this->mergeMenu($sn_menu_admin_extra, $this->manifest['menu_admin']);
+
+    $this->mergeJavascript();
+    $this->mergeCss();
+    $this->mergeNavbarButton();
+  }
+
+  protected function setSystemConstants() {
     // Setting constants - if any
-    if(isset($this->manifest['constants']) && is_array($this->manifest['constants']) && !empty($this->manifest['constants'])) {
-      foreach($this->manifest['constants'] as $constant_name => $constant_value) {
-        defined($constant_name) or define($constant_name, $constant_value);
-      }
+    if(empty($this->manifest['constants']) || !is_array($this->manifest['constants'])) {
+      return;
     }
 
+    foreach($this->manifest['constants'] as $constant_name => $constant_value) {
+      defined($constant_name) || define($constant_name, $constant_value);
+    }
+  }
+
+  protected function setSystemVariables() {
     // Adding vars - if any
     // Due to possible introduce of new constants in previous step vars is assigned via special method to honor new constants
     // Assignation can work with simple variables and with multidimensional arrays - for ex. 'sn_data[groups][test]'
     // New values from module variables will overwrite previous values (for root variables) and array elements with corresponding indexes (for arrays)
     // Constants as array indexes are honored - it's make valid such declarations as 'sn_data[ques][QUE_STRUCTURES]'
     $this->manifest['vars'] = $this->__assign_vars();
-    if(!empty($this->manifest['vars'])) {
-      $vars_assigned = array();
-      foreach($this->manifest['vars'] as $var_name => $var_value) {
-        $sub_vars = explode('[', str_replace(']', '', $var_name));
-        $var_name = $sub_vars[0];
-
-        if(!isset($vars_assigned[$var_name])) {
-          $vars_assigned[$var_name] = true;
-          global $$var_name;
-        }
-
-        $pointer = &$$var_name;
-        if(($n = count($sub_vars)) > 1) {
-          for($i = 1; $i < $n; $i++) {
-            if(defined($sub_vars[$i])) {
-              $sub_vars[$i] = constant($sub_vars[$i]);
-            }
-
-            if(!isset($pointer[$sub_vars[$i]]) && $i != $n) {
-              $pointer[$sub_vars[$i]] = array();
-            }
-            $pointer = &$pointer[$sub_vars[$i]];
-          }
-        }
-
-        if(!isset($pointer) || !is_array($pointer)) {
-          $pointer = $var_value;
-        } elseif(is_array($$var_name)) {
-          $pointer = array_merge_recursive_numeric($pointer, $var_value);
-        }
-      }
+    if(empty($this->manifest['vars']) || !is_array($this->manifest['vars'])) {
+      return;
     }
 
+    $vars_assigned = array();
+    foreach($this->manifest['vars'] as $var_name => $var_value) {
+      $sub_vars = explode('[', str_replace(']', '', $var_name));
+      $var_name = $sub_vars[0];
+
+      if(!isset($vars_assigned[$var_name])) {
+        $vars_assigned[$var_name] = true;
+        global $$var_name;
+      }
+
+      $pointer = &$$var_name;
+      if(($n = count($sub_vars)) > 1) {
+        for($i = 1; $i < $n; $i++) {
+          if(defined($sub_vars[$i])) {
+            $sub_vars[$i] = constant($sub_vars[$i]);
+          }
+
+          if(!isset($pointer[$sub_vars[$i]]) && $i != $n) {
+            $pointer[$sub_vars[$i]] = array();
+          }
+          $pointer = &$pointer[$sub_vars[$i]];
+        }
+      }
+
+      if(!isset($pointer) || !is_array($pointer)) {
+        $pointer = $var_value;
+      } elseif(is_array($$var_name)) {
+        $pointer = array_merge_recursive_numeric($pointer, $var_value);
+      }
+    }
+  }
+
+  protected function mergeMenu(&$sn_menu_extra, &$menu_patch) {
+    if(!is_array($menu_patch)) {
+      return;
+    }
+
+    foreach($menu_patch as $menu_item_name => $menu_item_data) {
+      $sn_menu_extra[$menu_item_name] = $menu_item_data;
+    }
+  }
+
+  protected function addSystemHandlers() {
+    global $sn_mvc, $functions;
+
     // Overriding function if any
-    global $functions;
     sn_sys_handler_add($functions, $this->manifest['functions'], $this);
 
-    // Patching game menu - if any
-    global $sn_menu_extra, $sn_menu_admin_extra;
-    isset($this->manifest['menu']) and $this->__patch_menu($sn_menu_extra, $this->manifest['menu']);
-    isset($this->manifest['menu_admin']) and $this->__patch_menu($sn_menu_admin_extra, $this->manifest['menu_admin']);
-
-    global $sn_mvc;
     foreach($sn_mvc as $handler_type => &$handler_data) {
       sn_sys_handler_add($handler_data, $this->manifest['mvc'][$handler_type], $this, $handler_type);
     }
+  }
 
-    if(isset($this->manifest['i18n']) && is_array($this->manifest['i18n']) && !empty($this->manifest['i18n'])) {
-      foreach($this->manifest['i18n'] as $i18n_page_name => &$i18n_file_list) {
-        foreach($i18n_file_list as &$i18n_file_data) {
-          if(is_array($i18n_file_data) && !$i18n_file_data['path']) {
-            $i18n_file_data['path'] = $this->manifest['root_relative'];
-          }
-        }
-        if(!isset($sn_mvc['i18n'][$i18n_page_name])) {
-          $sn_mvc['i18n'][$i18n_page_name] = array();
-        }
-        $sn_mvc['i18n'][$i18n_page_name] += $i18n_file_list;
-      }
+  protected function mergeNavbarButton() {
+    global $sn_mvc;
+
+    if(empty($this->manifest['navbar_prefix_button']) || !is_array($this->manifest['navbar_prefix_button'])) {
+      return;
     }
 
-    if(!empty($this->manifest['javascript']) && is_array($this->manifest['javascript'])) {
-      foreach($this->manifest['javascript'] as $javascript_page_name => &$javascript_list) {
-        !isset($sn_mvc['javascript'][$javascript_page_name]) ? $sn_mvc['javascript'][$javascript_page_name] = array() : false;
-        foreach($javascript_list as $script_name => &$script_content) {
-          $sn_mvc['javascript'][$javascript_page_name][$script_name] = $script_content;
-        }
-      }
+    foreach($this->manifest['navbar_prefix_button'] as $button_image => $button_url_relative) {
+      $sn_mvc['navbar_prefix_button'][$button_image] = $button_url_relative;
+    }
+  }
+
+  protected function mergeI18N() {
+    global $sn_mvc;
+
+    $arrayName = 'i18n';
+    if(empty($this->manifest[$arrayName]) || !is_array($this->manifest[$arrayName])) {
+      return;
     }
 
-    if(!empty($this->manifest['css']) && is_array($this->manifest['css'])) {
-      foreach($this->manifest['css'] as $javascript_page_name => &$javascript_list) {
-        !isset($sn_mvc['css'][$javascript_page_name]) ? $sn_mvc['css'][$javascript_page_name] = array() : false;
-        foreach($javascript_list as $script_name => &$script_content) {
-          $sn_mvc['css'][$javascript_page_name][$script_name] = $script_content;
+    foreach($this->manifest[$arrayName] as $pageName => &$contentList) {
+      foreach($contentList as &$i18n_file_data) {
+        if(is_array($i18n_file_data) && !$i18n_file_data['path']) {
+          $i18n_file_data['path'] = $this->manifest['root_relative'];
         }
       }
+      if(!isset($sn_mvc[$arrayName][$pageName])) {
+        $sn_mvc[$arrayName][$pageName] = array();
+      }
+      $sn_mvc[$arrayName][$pageName] += $contentList;
+    }
+  }
+
+  protected function mergeArraySpecial($arrayName) {
+    global $sn_mvc;
+
+    if(empty($this->manifest[$arrayName]) || !is_array($this->manifest[$arrayName])) {
+      return;
     }
 
-    if(!empty($this->manifest['navbar_prefix_button']) && is_array($this->manifest['navbar_prefix_button'])) {
-      foreach($this->manifest['navbar_prefix_button'] as $button_image => $button_url_relative) {
-        $sn_mvc['navbar_prefix_button'][$button_image] = $button_url_relative;
+    foreach($this->manifest[$arrayName] as $pageName => &$contentList) {
+      !isset($sn_mvc[$arrayName][$pageName]) ? $sn_mvc[$arrayName][$pageName] = array() : false;
+      foreach($contentList as $contentName => &$content) {
+        $sn_mvc[$arrayName][$pageName][$contentName] = $content;
       }
     }
   }
 
-  function check_status() {
-  }
+  protected function mergeCss() { $this->mergeArraySpecial('css'); }
+
+  protected function mergeJavascript() { $this->mergeArraySpecial('javascript'); }
+
+  function check_status() { }
+
 }
-
