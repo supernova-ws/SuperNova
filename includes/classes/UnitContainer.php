5ecb7d208dd5ed30badcf309e52a6ef2637cb8b3
Gorlum <gorlum@supernova.ws>
2016-03-27 20:43:54 +0300

    CODE-fix-41a6.72: Fixed wrong location of code - in UnitContainer instead of Player

--- includes/classes/UnitContainer.php
+++ includes/classes/UnitContainer.php
@@ -23,12 +23,6 @@ abstract class UnitContainer extends DBRowLocation {
   public function dbRowParse($db_row) {
     parent::dbRowParse($db_row);
     $this->unitList->dbLoad($this->_dbId, $this->lockSkip);
-
-    // Высчитываем бонусы
-    $this->player_bonus->add_unit_by_snid(MRC_ADMIRAL, mrc_get_level($this->db_row, null, MRC_ADMIRAL));
-    $this->player_bonus->add_unit_by_snid(TECH_WEAPON, mrc_get_level($this->db_row, null, TECH_WEAPON));
-    $this->player_bonus->add_unit_by_snid(TECH_SHIELD, mrc_get_level($this->db_row, null, TECH_SHIELD));
-    $this->player_bonus->add_unit_by_snid(TECH_ARMOR, mrc_get_level($this->db_row, null, TECH_ARMOR));
   }
 
 }
