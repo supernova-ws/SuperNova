a017eddc5be2e0d47eb9215e03353dd3d279565e
Gorlum <gorlum@supernova.ws>
2016-06-01 17:21:38 +0300

    TEST-fix-41a7.67: Really added tests

---
+++ tests/DBRowTest.php
@@ -0,0 +1,120 @@
+<?php
+
+class DBRowPublish extends DBRow {
+  public function dbGetLockById($dbId) {
+    // TODO: Implement dbGetLockById() method.
+  }
+
+  public function isEmpty() {
+    // TODO: Implement isEmpty() method.
+  }
+
+}
+
+/**
+ * Class DBRowTest
+ *
+ * @coversDefaultClass DBRow
+ */
+class DBRowTest extends PHPUnit_Framework_TestCase {
+  /**
+   * @var DBRowPublish $object
+   */
+  protected $object;
+
+  public function setUp() {
+    parent::setUp();
+    $this->object = new DBRowPublish(null);
+  }
+
+  public function tearDown() {
+    unset($this->object);
+    parent::tearDown();
+  }
+
+  /**
+   * @covers ::getDb
+   * @covers ::setDb
+   */
+  public function testSetDb() {
+    // Default values check
+    $this->assertEquals(null, DBRowPublish::getDb());
+
+    // Empty value check
+    DBRowPublish::setDb('');
+    $this->assertEquals(null, DBRowPublish::getDb());
+
+    // Non-empty non-object value check
+    DBRowPublish::setDb('qwe');
+    $this->assertEquals(null, DBRowPublish::getDb());
+
+    // Non-empty non-db_mysql value check
+    DBRowPublish::setDb(new stdClass());
+    $this->assertEquals(null, DBRowPublish::getDb());
+
+    // db_mysql value check. Also check that getter return value - not 'null' every time
+    $db = new db_mysql();
+    DBRowPublish::setDb($db);
+    $this->assertEquals($db, DBRowPublish::getDb());
+
+    // Null again
+    DBRowPublish::setDb(null);
+    $this->assertEquals(null, DBRowPublish::getDb());
+  }
+
+  /**
+   * @covers ::getTable
+   * @covers ::setTable
+   */
+  public function testSetTable() {
+    $this->assertEquals('', DBRowPublish::getTable());
+
+    DBRowPublish::setTable('table');
+    $this->assertEquals('table', DBRowPublish::getTable());
+  }
+
+  /**
+   * @covers ::getIdFieldName
+   * @covers ::setIdFieldName
+   */
+  public function testSetIdFieldName() {
+    $this->assertEquals('id', DBRowPublish::getIdFieldName());
+
+    DBRowPublish::setIdFieldName('id2');
+    $this->assertEquals('id2', DBRowPublish::getIdFieldName());
+  }
+
+  /**
+   * @covers ::getIdFieldName
+   * @covers ::setProperties
+   */
+  public function testSetProperties() {
+    $this->assertEquals(
+      array(
+        'dbId' => array(
+          P_DB_FIELD => 'id',
+        ),
+      ),
+      DBRowPublish::getProperties()
+    );
+
+    DBRowPublish::setProperties(array('test' => 'qwe'));
+    $this->assertEquals(array('test' => 'qwe'), DBRowPublish::getProperties());
+  }
+
+  /**
+   * @covers ::__construct
+   * @covers ::getDb
+   * covers ::build
+   */
+  public function test__construct() {
+    $db = new db_mysql();
+    $test = new DBRowPublish($db);
+
+    $this->assertEquals('DBRowPublish', get_class($test));
+    $this->assertEquals($db, $test->getDb());
+
+    unset($test);
+  }
+
+}
