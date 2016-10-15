6dcfe483a0e64ea1c618f41737e29e96c0f4f9f7
Gorlum <gorlum@supernova.ws>
2016-10-15 14:42:16 +0300

    CODE-misc-41a60.7: Reformat code a bit

--- announce.php
+++ announce.php
@@ -3,9 +3,7 @@
 /**
  * announce.php
  *
- * @v4 Security checks by Gorlum for http://supernova.ws
- * @v2 (c) copyright 2010 by Gorlum for http://supernova.ws
- * based on admin/activeplanet.php (c) 2008 for XNova
+ * @copyright (c) 2010-2016 Gorlum for http://supernova.ws
  */
 
 use DBStatic\DBStaticMessages;
@@ -28,40 +26,40 @@ $detail_url_unsafe = sys_get_param_str_unsafe('detail_url');
 $mode = sys_get_param_str('mode');
 
 $announce = array();
-if($user['authlevel'] >= 3) {
-  if(!empty($text)) {
+if ($user['authlevel'] >= 3) {
+  if (!empty($text)) {
     $announce_time = strtotime($announce_time, SN_TIME_NOW);
     $announce_time = $announce_time ? $announce_time : SN_TIME_NOW;
 
-    if($mode == 'edit') {
+    if ($mode == 'edit') {
       DBStaticNews::db_news_update_set($announce_time, $text_unsafe, $detail_url_unsafe, $announce_id);
       DBStaticSurvey::db_survey_delete_by_id($announce_id);
     } else {
       DBStaticNews::db_news_insert_set($announce_time, $text_unsafe, $detail_url_unsafe, $user['id'], $user['username']);
       $announce_id = classSupernova::$db->db_insert_id();
     }
-    if(($survey_question = sys_get_param_str('survey_question')) && ($survey_answers = sys_get_param('survey_answers'))) {
+    if (($survey_question = sys_get_param_str('survey_question')) && ($survey_answers = sys_get_param('survey_answers'))) {
       $survey_answers = explode("\r\n", $survey_answers);
       $survey_until = strtotime($survey_until = sys_get_param_str('survey_until'), SN_TIME_NOW);
       $survey_until = date(FMT_DATE_TIME_SQL, $survey_until ? $survey_until : SN_TIME_NOW + PERIOD_DAY * 1);
       $survey_question_unsafe = sys_get_param_str_unsafe('survey_question');
       DBStaticSurvey::db_survey_insert($announce_id, $survey_question_unsafe, $survey_until);
       $survey_id = classSupernova::$db->db_insert_id();
-      foreach($survey_answers as $survey_answer) {
+      foreach ($survey_answers as $survey_answer) {
         $survey_answer_unsafe = trim($survey_answer);
-        if(empty($survey_answer_unsafe)) {
+        if (empty($survey_answer_unsafe)) {
           continue;
         }
         DBStaticSurveyAnswer::db_survey_answer_insert($survey_id, $survey_answer_unsafe);
       }
     }
 
-    if($announce_time <= SN_TIME_NOW) {
-      if($announce_time > classSupernova::$config->var_news_last && $announce_time == SN_TIME_NOW) {
+    if ($announce_time <= SN_TIME_NOW) {
+      if ($announce_time > classSupernova::$config->var_news_last && $announce_time == SN_TIME_NOW) {
         classSupernova::$config->db_saveItem('var_news_last', $announce_time);
       }
 
-      if(sys_get_param_int('news_mass_mail')) {
+      if (sys_get_param_int('news_mass_mail')) {
         $lang_news_more = classLocale::$lang['news_more'];
         $text = sys_get_param('text') . ($detail_url ? " <a href=\"{$detail_url}\"><span class=\"positive\">{$lang_news_more}</span></a>" : '');
         DBStaticMessages::msgSendFromAdmin('*', classLocale::$lang['news_title'], $text);
@@ -73,7 +71,7 @@ if($user['authlevel'] >= 3) {
   }
 
   $survey_answers = '';
-  switch($mode) {
+  switch ($mode) {
     case 'del':
       DBStaticNews::db_news_delete_by_id($announce_id);
       $mode = '';
@@ -84,9 +82,9 @@ if($user['authlevel'] >= 3) {
       $template->assign_var('ID', $announce_id);
     case 'copy':
       $announce = DBStaticNews::db_news_with_survey_select_by_id($announce_id);
-      if($announce['survey_id']) {
+      if ($announce['survey_id']) {
         $query = DBStaticSurveyAnswer::db_survey_answer_text_select_by_news($announce);
-        while($row = db_fetch($query)) {
+        while ($row = db_fetch($query)) {
           $survey_answers[] = $row['survey_answer_text'];
         }
         $survey_answers = implode("\r\n", $survey_answers);
