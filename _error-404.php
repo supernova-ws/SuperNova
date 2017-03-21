37cf8ce102bc2951f8a4e352e29f7b45dae1b75a
Gorlum <gorlum@supernova.ws>
2017-03-21 19:15:54 +0200

    CODE-misc-42a27.10: Minor adjustments

--- _error-404.php
+++ _error-404.php
@@ -1,9 +1,9 @@
 /* <?php
 $redirectFrom = !empty($_SERVER["REDIRECT_URL"]) ? $_SERVER["REDIRECT_URL"] : '';
-$serverProtocol = !empty($_SERVER["SERVER_PROTOCOL"]) ? $_SERVER["SERVER_PROTOCOL"] : 'HTTP/1.0';
+// $serverProtocol = !empty($_SERVER["SERVER_PROTOCOL"]) ? $_SERVER["SERVER_PROTOCOL"] : 'HTTP/1.0';
 
 // header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
-@header("{$serverProtocol} 200 OK");
+// @header("{$serverProtocol} 200 OK");
 echo "File {$redirectFrom} not found on server. Contact administration if you think that this is error";
 die();
 ?> */
