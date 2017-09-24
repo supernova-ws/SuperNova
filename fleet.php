4657855d8ed04a0989aa289017cac79a1b972895
Gorlum <gorlum@supernova.ws>
2017-09-25 02:52:56 +0300

    CODE-fix-43a1.28: Changed code to improve quality

--- fleet.php
+++ fleet.php
@@ -77,7 +77,7 @@ switch ($fleet_page) {
 
   case 2:
     $fleet_group_mr = sys_get_param_id('fleet_group');
-    $fleetarray     = unserialize(base64_decode(str_rot13(sys_get_param('usedfleet'))));
+    $fleetarray     = json_decode(base64_decode(str_rot13(sys_get_param('usedfleet'))), true);
     $fleetarray = is_array($fleetarray) ? $fleetarray : array();
 
     foreach($fleetarray as $ship_id => &$ship_amount) {
