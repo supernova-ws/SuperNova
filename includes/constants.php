9e12d0a1147c705b23dc2e410663b5b493cd2e19
Gorlum <gorlum@supernova.ws>
2018-03-23 04:57:27 +0200

    DOC-43a16.7: Changelog editing

--- includes/constants.php
+++ includes/constants.php
@@ -10,7 +10,7 @@ defined('INSIDE') or die('Hacking attempt');
 
 define('DB_VERSION', '42');
 define('SN_RELEASE', '42');
-define('SN_VERSION', '43a16.3');
+define('SN_VERSION', '43a16.7');
 define('SN_RELEASE_STABLE', '42c2'); // Latest stable release
 
 define('SN_TIME_NOW', intval(SN_TIME_MICRO));
